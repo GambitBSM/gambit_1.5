99116d21916bc6b53bf7192f93148a5e703b4588
Hugh Dickinson <hugh.dickinson@fysik.su.se>
2012-12-04 15:43:04 +0100

    Added files for FortranFunctor and FortranInterface

--- extras/Hughs_Wrappers/FortranInterface.C
+++ extras/Hughs_Wrappers/FortranInterface.C
@@ -6,4 +6,27 @@
 //  Copyright (c) 2012 Hugh Dickinson. All rights reserved.
 //
 
-#include <stdio.h>
+#include "FortranInterface.h"
+
+/** Constructor.
+ */
+FortranInterface::FortranInterface(std::string const & libFileName):
+fFortranLibraryHandle(dlopen(libFileName.c_str(), RTLD_LAZY))
+{
+	CheckDLStatus();
+}
+
+/** Destructor
+ */
+FortranInterface::~FortranInterface(){
+	dlclose(fFortranLibraryHandle);
+}
+
+/** Check for dynamic library errors
+ */
+void FortranInterface::CheckDLStatus() const {
+	const char * error = dlerror();
+	if(error){
+		throw(FortranFunctorException(error));
+	}
+}
