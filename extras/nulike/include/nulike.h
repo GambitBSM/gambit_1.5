0eba60ef44763177a5ac8684b0fc9e271e712699
Pat Scott <patscott@physics.mcgill.ca>
2014-06-21 13:22:23 -0400

    Partial update of nulike.
    	modified:   contrib/TSPACK/SIGS.f
    	modified:   data/IceCube/likelihood2014/IC79_Background_distributions_WH.dat
    	modified:   data/IceCube/likelihood2014/IC79_Effective_Volume_WH.dat
    	modified:   data/IceCube/likelihood2014/IC79_Events_WH_10degrees.dat
    	new file:   data/IceCube/likelihood2014/IC79_Partial_Likelihoods_WH/effective_area.dat
    	new file:   data/IceCube/likelihood2014/IC79_Partial_Likelihoods_WH/partlike_event1.dat
    	new file:   data/IceCube/likelihood2014/IC79_Partial_Likelihoods_WH/partlike_event2.dat
    	new file:   data/IceCube/likelihood2014/IC79_Partial_Likelihoods_WH/partlike_event3.dat
    	new file:   data/IceCube/likelihood2014/IC79_Partial_Likelihoods_WH/partlikes.aux
    	modified:   data/IceCube/likelihood2014/IC79_energy_histograms_WH.dat
    	modified:   include/nulike.h
    	deleted:    src/edispcheckout.f
    	renamed:    src/effarea.f -> src/sens.f
    	renamed:    src/eainit.f -> src/sensinit.f
    	deleted:    test/nulike_test.f
    	modified:   ../../modules/TODO

--- extras/nulike/include/nulike.h
+++ extras/nulike/include/nulike.h
@@ -7,116 +7,108 @@
 !***                           nulike.h                               ***
 !***         this piece of code is needed as a separate file          ***
 !----------------------------------------------------------------------c
-!  author: Pat Scott (patscott@physics.mcgill.ca), March 26 2011
+!  author: Pat Scott (patscott@physics.mcgill.ca), March 26 2011, June 3, 6 2014
 
-      integer lun, ridiculousNumberOfChannels
-      integer max_nBinsEA,max_nBinsBGAng,max_nBinsBGE,max_nEvents
-      integer max_nHistograms,max_nnchan,nchan_maxallowed
-      integer angular, nchannels, events
-      character (len=15) hstring(3), nulike_version
+      include 'nucommon.h'
 
-      parameter(lun = 20)
-      parameter(ridiculousNumberOfChannels = 1000)
-      parameter(max_nBinsEA     = 10)
-      parameter(max_nBinsBGAng  = 180)
-      parameter(max_nBinsBGE    = 20)
-      parameter(max_nEvents     = 2000)
-      parameter(max_nHistograms = 20)
-      parameter(max_nnchan      = 23) !Max number of different nchan values for 2012 likelihood
-      parameter(nchan_maxallowed=100) !Maximum value of nchan
-      parameter(angular = 1, nchannels = 2, events = 3)
-      parameter(hstring = (/'####--Angular--',
-     &                      '####--Nchan--  ',
-     &                      '####--Nevents--'/))
+      integer nchan_maxallowed
+      parameter(nchan_maxallowed =100) !Maximum value of nchan
 
-      real*8 pi, bigBadLike
-      parameter (pi=3.141592653589793238d0)
-      parameter(bigBadLike = -50.d0)
+      real*8 effZero, logZero
+      parameter(effZero = 1.d-100)
+      parameter(logZero = log10(effZero))
 
-      integer nBinsEA,nBinsEAError,nBinsBGAng,nBinsBGE,nEvents
-      integer nHistograms
-      integer nchan_min, nchan_max, nnchan_total
+      character (len=100) analysis_name_array(max_analyses)
+      integer likelihood_version(max_analyses)
+      logical sysErrDist_logNorm(max_analyses)
+      real*8 phi_max_deg(max_analyses)
+      real*8 exp_time(max_analyses), theoryErr(max_analyses) 
+      real*8 theta_BG(max_analyses), EAErr(max_analyses)
 
-      real*8 effArea_logE(2,max_nBinsEA)
-      real*8 effArea_logEcentres(max_nBinsEA)
-      real*8 effArea_nu(max_nBinsEA)
-      real*8 effArea_nubar(max_nBinsEA)
-      real*8 effArea_syserr(max_nBinsEA)
-      real*8 effArea_staterr(max_nBinsEA)
-      real*8 effArea_AngRes(max_nBinsEA)
-      real*8 effArea_nuderivs(max_nBinsEA)
-      real*8 effArea_nubarderivs(max_nBinsEA)
-      real*8 effArea_AngResderivs(max_nBinsEA)
-      real*8 effArea_nusigma(max_nBinsEA)
-      real*8 effArea_nubarsigma(max_nBinsEA)
-      real*8 effArea_AngRessigma(max_nBinsEA)
+      integer nSensBins(max_analyses)
+      integer nBinsBGAng(max_analyses), nBinsBGE(max_analyses)
+      integer nEvents(max_analyses), nEvents_in_file(max_analyses)
+      integer nHistograms(max_analyses), nnchan_total(max_analyses)
+      real*8  ee_min(max_analyses), ee_max(max_analyses) 
 
-      real*8 EAlogE_inEAErrBins(2,max_nBinsEA)
-      real*8 EAErr_inEAErrBins(max_nBinsEA)
-      integer nEvents_inEAErrBins(max_nBinsEA)
-      integer maxEAErrIndex
+      real*8 sens_logE(2,max_nSensBins,max_analyses)
+      real*8 sens_logEcentres(max_nSensBins,max_analyses)
+      real*8 sens_nu(max_nSensBins,max_analyses)
+      real*8 sens_nubar(max_nSensBins,max_analyses)
+      real*8 sens_syserr(max_nSensBins,max_analyses)
+      real*8 sens_staterr(max_nSensBins,max_analyses)
+      real*8 sens_AngRes(max_nSensBins,max_analyses)
+      real*8 sens_nuderivs(max_nSensBins,max_analyses)
+      real*8 sens_nubarderivs(max_nSensBins,max_analyses)
+      real*8 sens_AngResderivs(max_nSensBins,max_analyses)
+      real*8 sens_nusigma(max_nSensBins,max_analyses)
+      real*8 sens_nubarsigma(max_nSensBins,max_analyses)
+      real*8 sens_AngRessigma(max_nSensBins,max_analyses)
 
-      real*8  BGangdist_phi(max_nBinsBGAng)
-      real*8  BGangdist_prob(max_nBinsBGAng)
-      real*8  BGangdist_derivs(max_nBinsBGAng)
-      real*8  BGangdist_sigma(max_nBinsBGAng)
-      real*8  BGangdist_norm
-      integer BGnchandist_nchan(max_nBinsBGE)
-      real*8  BGnchandist_prob(max_nBinsBGE)
+      real*8  BGangdist_phi(max_nBinsBGAng,max_analyses)
+      real*8  BGangdist_prob(max_nBinsBGAng,max_analyses)
+      real*8  BGangdist_derivs(max_nBinsBGAng,max_analyses)
+      real*8  BGangdist_sigma(max_nBinsBGAng,max_analyses)
+      real*8  BGangdist_norm(max_analyses), BGangdist_conenorm(max_analyses)
+      real*8  BGeedist_ee(max_nBinsBGE,max_analyses)
+      real*8  BGeedist_prob(max_nBinsBGE,max_analyses)
+      real*8  BGeedist_derivs(max_nBinsBGE,max_analyses)
+      real*8  BGeedist_sigma(max_nBinsBGE,max_analyses)
+      integer FullSkyBG(max_analyses)
 
-      real*8  hist_logE(2,max_nHistograms)
-      real*8  hist_logEcentres(max_nHistograms)
-      integer hist_nchan(max_nHistograms, max_nnchan)
-      real*8  hist_prob(max_nHistograms, max_nnchan)
-      real*8  hist_derivs(max_nHistograms, max_nnchan)
-      real*8  hist_sigma(max_nHistograms, max_nnchan)
-      integer bestGuessBin(max_nnchan)
-      real*8  relProb(max_nnchan,max_nBinsEA)
-      integer nchan_hist2BGoffset
-      real*8  edisp_prob(max_nHistograms)
-      real*8  edisp_derivs(max_nHistograms)
-      real*8  edisp_sigma(max_nHistograms)
+      real*8  hist_logE(2,max_nHistograms,max_analyses)
+      real*8  hist_logEcentres(max_nHistograms,max_analyses)
+      integer hist_nchan(max_nHistograms,max_ncols,max_analyses)
+      real*8  hist_prob(max_nHistograms,max_ncols,max_analyses)
+      real*8  hist_derivs(max_nHistograms,max_ncols,max_analyses)
+      real*8  hist_sigma(max_nHistograms,max_ncols,max_analyses)
+      integer nchan_hist2BGoffset(max_analyses)
 
-      integer events_nchan(max_nBinsEA,max_nEvents)
-      real*8  events_cosphi(max_nBinsEA,max_nEvents)
-      real*8  events_cosphiErr(max_nBinsEA,max_nEvents)
+      real*8  events_nchan(max_nEvents,max_analyses)
+      real*8  events_cosphi(max_nEvents,max_analyses)
+      real*8  events_cosphiErr(max_nEvents,max_analyses)
 
-      real*8  theta_BG(max_nBinsEA)
-      real*8  theta_S(max_nBinsEA)
-      real*8  theta_Snu(max_nBinsEA)
-      real*8  theta_Snubar(max_nBinsEA)
-      real*8  phi_max_rad, phi_max_deg, exp_time, theta_S_total
-      real*8  Eshare, thetashare, log10mwimp, theoryErr, EAErr_max
-      real*8  BGpvalPoissonian, annrate, BGangdist_conenorm
-      integer FullSkyBG, ptypeshare, nchanshare, nchansaved
-      logical pvalBGPoisComputed, sysErrDist_logNorm
+      logical pvalBGPoisComputed(max_analyses)
+      real*8  BGpvalPoissonian(max_analyses)
 
-      common /nulike_comm/ events_nchan,events_cosphi,events_cosphiErr,
-     & effArea_logE,EAlogE_inEAErrBins,effArea_nu,relProb,theta_BG,
-     & theta_S,EAErr_inEAErrBins,nEvents_inEAErrBins, BGnchandist_prob,
-     & effArea_logEcentres, effArea_nuderivs, effArea_nubarderivs,
-     & effArea_AngResderivs, effArea_nusigma, effArea_nubarsigma,
-     & effArea_AngRessigma, BGangdist_derivs, BGangdist_sigma,
-     & BGangdist_phi, BGangdist_prob, BGnchandist_nchan, phi_max_rad,
-     & phi_max_deg, log10mwimp, theoryErr, EAErr_max, BGpvalPoissonian,
-     & BGangdist_norm, effArea_AngRes, exp_time, Eshare, thetashare, 
-     & theta_Snu, theta_Snubar, theta_S_total, annrate, 
+      integer nPrecompE(max_nPrecompE)
+      real*8 precomp_log10E(max_nPrecompE,max_analyses)
+      real*8 precomp_weights(max_nPrecompE,max_nEvents,2,max_analyses)
+      real*8 precomp_derivs(max_nPrecompE,max_nEvents,2,max_analyses)
+      real*8 precomp_sigma(max_nPrecompE,max_nEvents,2,max_analyses)
+      real*8 precompEA_weights(max_nPrecompE,2,max_analyses)
+      real*8 precompEA_derivs(max_nPrecompE,2,max_analyses)
+      real*8 precompEA_sigma(max_nPrecompE,2,max_analyses)
+
+      real*8  thetashare, annrateshare, nchanshare
+
+      common /nulike_comm/ events_nchan, events_cosphi,
+     & events_cosphiErr, sens_logE,sens_nu,theta_BG, BGeedist_prob,
+     & sens_logEcentres, sens_nuderivs, sens_nubarderivs,
+     & sens_AngResderivs, sens_nusigma, sens_nubarsigma,
+     & sens_AngRessigma, BGangdist_derivs, BGangdist_sigma,
+     & BGangdist_phi, BGangdist_prob, BGeedist_ee,
+     & phi_max_deg, theoryErr, EAErr, BGpvalPoissonian, BGangdist_norm,
+     & sens_AngRes, exp_time, thetashare, annrateshare, 
      & BGangdist_conenorm, hist_LogE, hist_logEcentres, hist_nchan,
-     & hist_prob, hist_derivs, hist_sigma, edisp_prob, edisp_derivs,
-     & edisp_sigma, effArea_nubar,effArea_syserr,effArea_staterr,
-     & bestGuessBin, nBinsEA, nBinsEAError, nBinsBGAng,nBinsBGE,nEvents,
-     & nHistograms, nnchan_total, nchan_min, nchan_max, maxEAErrIndex,
-     & nchan_hist2BGoffset, FullSkyBG, ptypeshare, nchanshare,
-     & nchansaved, pvalBGPoisComputed,
-     & sysErrDist_logNorm, nulike_version
+     & hist_prob, hist_derivs, hist_sigma, sens_nubar,sens_syserr,
+     & sens_staterr, precomp_log10E, precomp_weights, precomp_derivs,
+     & precomp_sigma, precompEA_weights, precompEA_derivs, 
+     & precompEA_sigma, ee_min, ee_max, nchanshare,
+     & BGeedist_derivs, BGeedist_sigma,
+     & nBinsBGE, nBinsBGAng, nEvents, nEvents_in_file,
+     & nSensBins, nPrecompE, nHistograms, nnchan_total,
+     & nchan_hist2BGoffset, FullSkyBG,
+     & pvalBGPoisComputed, sysErrDist_logNorm,
+     & analysis_name_array, likelihood_version
       save /nulike_comm/
 
-      ! These parameters will be initialized in a block data routine
-      ! (nulike_FlagBlock, see init.f).  The external line below
-      ! ensures the block data routine is loaded at compile time.
+      ! This parameter will be initialized in a block data routine
+      ! (see flagblocks.f).  The external line below ensures the 
+      ! block data routine is loaded at compile time.
       logical nulike_init_called     
-      common /nulike_flags/ nulike_init_called
-      save /nulike_flags/
-      external nulike_FlagBlock
+      common /nulike_init_flag/ nulike_init_called
+      save /nulike_init_flag/
+      external nulike_init_flagblock
 
 !*************************** end of nulike.h *****************************
