08234616172fe630f292c0e4b4510265923dca1f
Pat Scott <patscott@physics.mcgill.ca>
2014-06-27 14:43:30 -0400

    Made nested abstract class example slightly more instructive.
    	modified:   example.cpp

--- extras/nested_virtual_example/example.cpp
+++ extras/nested_virtual_example/example.cpp
@@ -19,14 +19,14 @@ int main()
   specialised_outer<int> int_example;
 
   //Set all the internal values in both objects to the same values.
-  dbl_example.Insides1.set_mA(0.5);
-  dbl_example.Insides2.set_mB(0.5);
-  int_example.Insides1.set_mA(0.5);
-  int_example.Insides2.set_mB(0.5);
+  dbl_example.Insides1.set_mA(0.4);
+  dbl_example.Insides2.set_mB(0.4);
+  int_example.Insides1.set_mA(0.4);
+  int_example.Insides2.set_mB(0.4);
 
   //Print the functions of mA and mB, in each object.
-  std::cout << dbl_example.Insides1.get_f_of_mA(1.0) << std::endl;
-  std::cout << dbl_example.Insides2.get_f_of_mB(1.0, 1.0) << std::endl;
+  std::cout << dbl_example.Insides1.get_f_of_mA(1.1) << std::endl;
+  std::cout << dbl_example.Insides2.get_f_of_mB(1.1, 1.1) << std::endl;
   std::cout << int_example.Insides1.get_f_of_mA(1.1) << std::endl;
   std::cout << int_example.Insides2.get_f_of_mB(1.1, 1.1) << std::endl;
 
