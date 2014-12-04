fea5e0aecb156181810c5fd916e207a15e8c6827
Christoph Weniger <c.weniger@uva.nl>
2014-12-04 09:48:17 +0100

    Add more generalized function stuff
    - Simple mapping on plain functions with void* pointer
    - Smart mapping on plain functions *without* void pointers
      (using "traits classes" or whatever)

--- extras/BaseFunctions/funk.cpp
+++ extras/BaseFunctions/funk.cpp
@@ -19,39 +19,67 @@ class FunkBase;
 typedef shared_ptr<FunkBase> FunkPtr;
 typedef std::map<const char*, double> FunkArgs;
 
+template <typename... Args>
+using PlainPtrs = std::pair<double(*)(Args...,void*), void*>;
+template <typename... Args>
+using PlainPtr = double(*)(Args...);
+
+inline std::vector<double> linspace(double x0, double x1, unsigned int n)
+{
+    std::vector<double> ret;
+    double dx = 0;
+    if (n > 1)
+        dx = (x1-x0)/(n-1);
+        for (unsigned int i = 0; i<n; i++)
+        {
+            ret.push_back(x0 + i * dx);
+        }
+    return ret;
+}
+
+// Generate logarithmic 1-dim grid
+inline std::vector<double> logspace(double x0, double x1, unsigned int n)
+{
+    std::vector<double> ret;
+    double dx = 0;
+    if (n > 1)
+        dx = (x1-x0)/(n-1);
+        for (unsigned int i = 0; i<n; i++)
+        {
+            ret.push_back(pow(10, x0 + i * dx));
+        }
+    return ret;
+}
 
 
 template <typename T>
-std::vector<T> initVector(std::vector<T> vector)
+std::vector<T> vec(std::vector<T> vector)
 {
     return vector;
 }
 template <typename T, typename... Args>
-std::vector<T> initVector(std::vector<T> vector, T value, Args... args)
+std::vector<T> vec(std::vector<T> vector, T value, Args... args)
 {
     vector.push_back(value);
-    return initVector(vector, args...);
+    return vec(vector, args...);
 }
 // This function causes a (readable) compile-time error when T != U.
 // In case types are convertable, they are converted.
 template <typename T, typename U, typename... Args>
-std::vector<T> initVector(std::vector<T> vector, U value, Args... args)
+std::vector<T> vec(std::vector<T> vector, U value, Args... args)
 {
     T value_converted = value;
     vector.push_back(value_converted);
-    return initVector(vector, args...);
+    return vec(vector, args...);
 }
 template <typename T, typename... Args>
-std::vector<T> initVector(T value, Args... args)
+std::vector<T> vec(T value, Args... args)
 {
     std::vector<T> vector;
     vector.push_back(value);
-    vector = initVector(vector, args...);
+    vector = vec(vector, args...);
     return vector;
 }
-
-
-
 
 class FunkBase: public enable_shared_from_this<FunkBase>
 {
@@ -64,7 +92,12 @@ class FunkBase: public enable_shared_from_this<FunkBase>
         template <typename... Args> FunkPtr set (Args... args);
         template <typename... Args> double eval (Args... args);
 
-        virtual double value(FunkArgs&) = 0;
+        PlainPtrs<double,double> plain(const char*, const char*);
+
+        template <typename T>
+        PlainPtr<double,double> plain(const char*, const char*);
+
+        virtual double value(const FunkArgs&) = 0;
 
     private:
         // Read argument list.
@@ -85,21 +118,64 @@ class FunkBase: public enable_shared_from_this<FunkBase>
         void digest_arguments() {};
 };
 
+class FunkPlain: public FunkBase
+{
+    public:
+        FunkPlain(FunkPtr f, const char* arg1, const char* arg2) : f(f), arg1(arg1), arg2(arg2) {};
+
+        static double plain2(double x1, double x2, void* ptr)
+        {
+            FunkPlain * funkPtrPtr = static_cast<FunkPlain*>(ptr);
+            return funkPtrPtr->eval(funkPtrPtr->arg1, x1, funkPtrPtr->arg2, x2);
+        }
+
+        template <typename T>
+        static double plain2(double x1, double x2)
+        {
+            FunkPlain * funkPtrPtr = static_cast<FunkPlain*>(T::ptr);
+            return funkPtrPtr->eval(funkPtrPtr->arg1, x1, funkPtrPtr->arg2, x2);
+        }
+
+        double value(const FunkArgs& args) 
+        { 
+            return f->value(args);
+        }
+
+    private:
+        FunkPtr f;
+        const char* arg1;
+        const char* arg2;
+};
+
+PlainPtrs<double,double> FunkBase::plain(const char* arg1, const char* arg2)
+{
+    void* ptr = new FunkPlain(shared_from_this(), arg1, arg2);
+    return PlainPtrs<double,double>(&FunkPlain::plain2, ptr);
+}
+
+template <typename T>
+PlainPtr<double,double> FunkBase::plain(const char* arg1, const char* arg2)
+{
+    T::ptr = new FunkPlain(shared_from_this(), arg1, arg2);
+    return &FunkPlain::plain2<T>;
+}
+
 class FunkDerived: public FunkBase
 {
     public:
         FunkDerived(FunkPtr f, FunkArgs argmap) : f(f), argmap(argmap) {};
         FunkDerived(FunkPtr f, FunkArgs argmap, std::map<const char*, FunkPtr> funmap) : f(f), argmap(argmap), funmap(funmap) {};
 
-        double value(FunkArgs & args)
+        double value(const FunkArgs & args)
         {
             // args dominate over myargs
+            FunkArgs myargs = args;  // Make copy
             for ( auto it = funmap.begin(); it != funmap.end(); it ++ )
             {
-                args[it->first] = (it->second)->value(args);
+                myargs[it->first] = (it->second)->value(myargs);
             }
-            args.insert(argmap.begin(), argmap.end());
-            return f->value(args);
+            myargs.insert(argmap.begin(), argmap.end());
+            return f->value(myargs);
         }
 
     private:
@@ -115,7 +191,7 @@ class FunkMath_##OPERATION: public FunkBase                                     
         FunkMath_##OPERATION(FunkPtr f1, FunkPtr f2) : f1(f1), f2(f2), mode(0) {};                 \
         FunkMath_##OPERATION(FunkPtr f,  double x) : f1(f) , x(x), mode(1) {};                     \
         FunkMath_##OPERATION(double x,  FunkPtr f) : f1(f),  x(x), mode(2) {};                     \
-        double value(FunkArgs& args)                                                               \
+        double value(const FunkArgs& args)                                                         \
         {                                                                                          \
             if ( mode == 0 ) return f1->value(args) SYMBOL f2->value(args);                        \
             else if( mode == 1) return f1->value(args) SYMBOL x;                                   \
@@ -142,7 +218,7 @@ class FunkMath_##MATH: public FunkBase                                          
         FunkMath_##MATH(FunkPtr f1, FunkPtr f2) : f1(f1), f2(f2), mode(0) {};                      \
         FunkMath_##MATH(FunkPtr f,  double x) : f1(f) , x(x), mode(1) {};                          \
         FunkMath_##MATH(double x,  FunkPtr f) : f1(f),  x(x), mode(2) {};                          \
-        double value(FunkArgs& args)                                                               \
+        double value(const FunkArgs& args)                                                         \
         {                                                                                          \
             if ( mode == 0 ) return MATH(f1->value(args), f2->value(args));                        \
             else if( mode == 1) return MATH(f1->value(args), x);                                   \
@@ -159,17 +235,19 @@ FunkPtr MATH (FunkPtr f1,  double x2) { return FunkPtr(new FunkMath_##MATH(f1, x
 MATH_OPERATION(pow)
 #undef MATH_OPERATION
 
-#define MATH_OPERATION(MATH)                                           \
-class FunkMath_##MATH: public FunkBase                                 \
-{                                                                      \
-    public:                                                            \
-        FunkMath_##MATH(FunkPtr f) : f(f) {};                          \
-        double value(FunkArgs& args) { return MATH(f->value(args)); }  \
-    private:                                                           \
-        FunkPtr f;                                                     \
-};                                                                     \
+#define MATH_OPERATION(MATH)                                                 \
+class FunkMath_##MATH: public FunkBase                                       \
+{                                                                            \
+    public:                                                                  \
+        FunkMath_##MATH(FunkPtr f) : f(f) {};                                \
+        double value(const FunkArgs& args) { return MATH(f->value(args)); }  \
+    private:                                                                 \
+        FunkPtr f;                                                           \
+};                                                                           \
 FunkPtr MATH(FunkPtr f) { return FunkPtr(new FunkMath_##MATH(f)); }
 MATH_OPERATION(log)
+MATH_OPERATION(sqrt)
+MATH_OPERATION(log10)
 MATH_OPERATION(sin)
 MATH_OPERATION(cos)
 MATH_OPERATION(tan)
@@ -192,19 +270,18 @@ template <typename... Args> double FunkBase::eval (Args... args)
     return value(tmp_argmap);
 }
 
-
 template <typename... funcargs>
-class FunkPlain: public FunkBase
+class FunkFunk: public FunkBase
 {
     public:
         template <typename... Args>
-        FunkPlain(double (*f)(funcargs...), Args... args)
+        FunkFunk(double (*f)(funcargs...), Args... args)
         {
             ptr = f;
-            argnames = initVector(args...);
+            argnames = vec(args...);
         }
 
-        double value(FunkArgs& arg)
+        double value(const FunkArgs& arg)
         {
             return value_from_map(arg);
         }
@@ -216,20 +293,21 @@ class FunkPlain: public FunkBase
 
         // Return value according to map
         template <typename... Args>
-        double value_from_map(FunkArgs & argmap, Args&... args)
+        double value_from_map(const FunkArgs & argmap, Args&... args)
         {
             auto it = argmap.find(argnames[sizeof...(args)]);
             if (it != argmap.end())
             {
-                return value_from_map(argmap, args..., it->second);
+                double x = it->second;
+                return value_from_map(argmap, args..., x);
             }
             else
             {
-                std::cout << "ERROR. Missing parameter " << argnames[sizeof...(args)] << std::endl;
+                std::cout << "ERROR. Missing parameter: " << argnames[sizeof...(args)] << std::endl;
                 exit(-1);
             }
         }
-        double value_from_map(FunkArgs & argmap, funcargs&... args)
+        double value_from_map(const FunkArgs & argmap, funcargs&... args)
         {
             return (*ptr)(args...);
         }
@@ -237,33 +315,32 @@ class FunkPlain: public FunkBase
 
 template <typename... funcargs, typename... Args>
 FunkPtr func(double (*f)(funcargs...), Args... args) {
-    return FunkPtr(new FunkPlain<funcargs...>(f, args...));
+    return FunkPtr(new FunkFunk<funcargs...>(f, args...));
 }
 
 class FunkVar: public FunkBase
 {
     public:
         FunkVar(const char* var) : var(var) {};
-        double value(FunkArgs& args) { return args[var]; }
+
+        double value(const FunkArgs& args) { 
+            auto it = args.find(var);
+            if (it != args.end())
+            {
+                return args.at(var); 
+            }
+            else
+            {
+                std::cout << "ERROR. Missing parameter: " << var << std::endl;
+                exit(-1);
+            }
+        }
 
     private:
         const char* var;
 };
 
 FunkPtr v(const char* var) { return FunkPtr(new FunkVar(var)); }
-
-
-
-
-double f(double x1, double x2)
-{
-    return x1 + x2*2;
-}
-
-double g(double x1, double x2, double x3, double x4)
-{
-    return pow(x1, 2) - pow(x2, 2) - pow(x3, 2) - pow(x4, 2);
-}
 
 class FunkInterp : public FunkBase
 {
@@ -277,11 +354,21 @@ class FunkInterp : public FunkBase
             else if ( mode == "log" ) this->ptr = &FunkInterp::logInterp;
         };
 
-        double value(FunkArgs& args)
+        double value(const FunkArgs& args)
         {
-            return (this->*ptr)(args[arg]);
+            auto it = args.find(arg);
+            if (it != args.end())
+            {
+                return (this->*ptr)(args.at(arg));
+            }
+            else
+            {
+                std::cout << "ERROR. Missing parameter: " << arg << std::endl;
+                exit(-1);
+            }
         }
 
+    private:
         double logInterp(double x)
         {
             // Linear interpolation in log-log space
@@ -306,7 +393,6 @@ class FunkInterp : public FunkBase
             return y0 + (x-x0)/(x1-x0)*(y1-y0);
         }
         
-    private:
         double(FunkInterp::*ptr)(double);
         const char * arg;
         std::vector<double> Xgrid;
@@ -317,16 +403,56 @@ class FunkInterp : public FunkBase
 FunkPtr interp(const char * arg, std::vector<double> x, std::vector<double> y) { return FunkPtr(new FunkInterp(arg, x, y)); }
 
 
+double f(double x1, double x2)
+{
+    return x1 + x2*2;
+}
+
+double g(double x1, double x2, double x3, double x4)
+{
+    return pow(x1, 2) - pow(x2, 2) - pow(x3, 2) - pow(x4, 2);
+}
+
+double h(double x)
+{
+    return sqrt(x);
+}
+
+#define DEFINE_FUNK_TRAIT(C) class C { public: static void* ptr; }; void* C::ptr = 0;
+
+DEFINE_FUNK_TRAIT(FUNK_F1)
+
 int main()
 {
-    //auto abs = funkPlain(f, "lambda", "mass");
-    //std::cout << abs("lambda", 4, "mass", 3) << std::endl;
     FunkPtr b = func(g, "x1", "x2", "x3", "x4")->set("x1", 3);
 
     FunkPtr n = v("x1") + 3;
     FunkPtr m = v("mass") + 3;
 
-    cos(m)->eval("x1", 3, "x2", 4);
+    std::cout << func(h, "x")->set("x", interp("x", vec(1., 2., 3.), vec(3., 4., 5.)))->eval("x", 2.121) << std::endl;
+    std::cout << log10(func(h, "x")->set("x", interp("x", linspace(1, 3, 10), linspace(20, 40, 10))))->eval("x", 2.121) << std::endl;
+    /*
+    std::cout << "1" << std::endl;
+    auto p = (pow(sin(v("x")),2) + pow(cos(v("y")),2)) * v("z");
+    for (int i = 0; i < 1000000; i++)
+    {
+        p->eval("x", 0.5, "y", 0.5, "z", 0.5);
+    }
+    std::cout << "1" << std::endl;
+    for (int i = 0; i < 1000000; i++)
+    {
+        ((pow(sin(v("x")),2) + pow(cos(v("y")),2)) * v("z"))->eval("x", 0.5, "y", 0.5, "z", 0.5);
+    }
+    std::cout << "1" << std::endl;
+    */
 
-    std::cout << interp("x", initVector(1., 2., 3.), initVector(3., 4., 5.))->eval("x", 2.121) << std::endl;
+    double(*p1)(double,double,void*);
+    void* p2;
+
+    std::tie(p1, p2) = (v("x")*v("y"))->plain("x", "y");
+    std::cout << p1(3, 2, p2) << std::endl;
+
+    double(*p3)(double,double);
+    p3 = (v("x")*v("y"))->plain<FUNK_F1>("x", "y");
+    std::cout << p3(4, 1) << std::endl;
 }
