85a0f21546af28a708952d6973b1074f6f1e45b0
Christoph Weniger <c.weniger@uva.nl>
2015-05-18 12:30:31 +0200

    Minor updates in funktions.hpp
    - Print constuction and destruction information for debugging purposes
    
    	modified:   ../../../../../extras/BaseFunctions/crash.cpp
    	modified:   funktions.hpp

--- extras/BaseFunctions/crash.cpp
+++ extras/BaseFunctions/crash.cpp
@@ -10,21 +10,6 @@ int main()
 {
     auto x = Funk::var("x");
     auto y = Funk::var("y");
-    auto z = Funk::var("z");
     auto f = x*y;
-    auto g = sin(x)*z;
-    auto h = y+3*exp(z);
     auto F = f->bind("x", "y");
-    auto G = g->bind("x", "z");
-    auto H = h->bind("y", "z");
-
-    std::vector<Funk::BoundFunk> v;
-    std::vector<Funk::BoundFunk> w;
-    for (int i=0; i<10000; i++)
-    {
-        auto I = f->bind("x", "y");
-        auto J = g->bind("x", "z");
-        v.push_back(I);
-        w.push_back(J);
-    }
 }
