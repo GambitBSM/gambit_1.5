122019c0f34328ca8addecf8421ffc0897dea3e9
Christoph Weniger <c.weniger@uva.nl>
2015-05-18 23:45:25 +0200

    A hopefully permanent fix for the segfault issues of funktion.hpp
    	modified:   ../../../extras/BaseFunctions/crash.cpp
    	modified:   ../../Elements/include/gambit/Elements/funktions.hpp

--- extras/BaseFunctions/crash.cpp
+++ extras/BaseFunctions/crash.cpp
@@ -6,10 +6,22 @@
 // This does not actually crash, but was supposed to reproduce a segfault from
 // the destructor of funktion objects.  Maybe it is useful later.
 
-int main()
+struct Dumb
 {
+    Funk::BoundFunk function;
+};
+
+Dumb makeDumb()
+{
+    Dumb dumb;
     auto x = Funk::var("x");
     auto y = Funk::var("y");
     auto f = x*y;
-    auto F = f->bind("x", "y");
+    dumb.function = f->bind("x", "y");
+    return dumb;
+}
+
+int main()
+{
+    Dumb d = makeDumb();
 }
