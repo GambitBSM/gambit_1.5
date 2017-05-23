a995620160f173a5bac034af2e44b65242430453
Tomas Gonzalo <t.e.gonzalo@fys.uio.no>
2017-05-23 16:42:01 +0200

    CosmoBit module

---
+++ CosmoBit/src/CosmoBit.cpp
@@ -0,0 +1,35 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  Function definitions of CosmoBit.
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///
+///  \author Tomas Gonzalo
+///          (t.e.gonzalo@fys.uio.no)
+///  \date 2017 Jan
+///
+///  *********************************************
+
+#include <algorithm>
+
+#include "gambit/Elements/gambit_module_headers.hpp"
+#include "gambit/PrecisionBit/CosmoBit_rollcall.hpp"
+
+
+namespace Gambit
+{
+
+  namespace CosmoBit
+  {
+
+    using namespace LogTags;
+
+    // Module functions
+
+
+  }
+}
