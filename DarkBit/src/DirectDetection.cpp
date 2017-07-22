680a3690a3dd1315958634a0e46231619fc6b810
Jonathan Cornell <cornellj@physics.mcgill.ca>
2017-07-22 12:22:02 -0400

    Added option to turn off MicrOmegas DM-gluon scattering via box.

--- DarkBit/src/DirectDetection.cpp
+++ DarkBit/src/DirectDetection.cpp
@@ -179,8 +179,12 @@ namespace Gambit {
         << (*BEreq::MOcommon).par[7] << EOM;
 
       double p1[2], p2[2], p3[2], p4[2];
-      int error = BEreq::nucleonAmplitudes(byVal(BEreq::FeScLoop.pointer()),
+      int error;
+      if (runOptions->getValueOrDef<bool>(true, "box"))
+        error = BEreq::nucleonAmplitudes(byVal(BEreq::FeScLoop.pointer()),
           byVal(p1), byVal(p2), byVal(p3), byVal(p4));
+      else error = BEreq::nucleonAmplitudes(NULL,
+              byVal(p1), byVal(p2), byVal(p3), byVal(p4));
       if(error!=0)
         DarkBit_error().raise(LOCAL_INFO,
             "micrOMEGAs nucleonAmplitudes function failed with "
