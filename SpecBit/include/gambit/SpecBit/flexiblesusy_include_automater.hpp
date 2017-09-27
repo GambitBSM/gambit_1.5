2ce3f4385e3e57d383d9f7151f3b83d8ab671a74
Peter Athron <peter.athron@coepp.org.au>
2017-09-27 13:15:33 +1000

    Delete two_scale from auto include of model hpp files as names chnaged (due tio necessary restructuring in FS for semin-anlytic solver)

--- SpecBit/include/gambit/SpecBit/flexiblesusy_include_automater.hpp
+++ SpecBit/include/gambit/SpecBit/flexiblesusy_include_automater.hpp
@@ -26,8 +26,8 @@
 #include INCLUDE_FILE(input_parameters.hpp)
 #include INCLUDE_FILE(slha_io.hpp)
 #include INCLUDE_FILE(spectrum_generator.hpp)
-#include INCLUDE_FILE(two_scale_model.hpp)
-#include INCLUDE_FILE(two_scale_model_slha.hpp)
+#include INCLUDE_FILE(model.hpp)
+#include INCLUDE_FILE(model_slha.hpp)
 #include INCLUDE_FILE(physical.hpp)
 #include INCLUDE_FILE(info.hpp)
 
