8d4a68e6da52f40563656c69410a3f1bffcc3b72
Peter Athron <peter.athron@coepp.org.au>
2017-09-28 18:14:12 +1000

    fix make distclean issue

--- contrib/MassSpectra/flexiblesusy/models/MSSMatMSUSY_mAmu/MSSMatMSUSY_mAmu_two_scale_EWSB_solution.m
+++
@@ -1,6 +0,0 @@
-{{mHd2 -> (-3*g1^2*vd^3 - 5*g2^2*vd^3 + 3*g1^2*vd*vu^2 + 5*g2^2*vd*vu^2 + 
-     20*vu*B[\[Mu]] - 40*vd*\[Mu]*conj[\[Mu]] + 20*vu*conj[B[\[Mu]]] + 
-     40*tadpole[1])/(40*vd), 
-  mHu2 -> (3*g1^2*vd^2*vu + 5*g2^2*vd^2*vu - 3*g1^2*vu^3 - 5*g2^2*vu^3 + 
-     20*vd*B[\[Mu]] - 40*vu*\[Mu]*conj[\[Mu]] + 20*vd*conj[B[\[Mu]]] + 
-     40*tadpole[2])/(40*vu)}}
