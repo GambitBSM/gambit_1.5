89cf8fdef3d59616454d093a239a22d180319688
Pat Scott <p.scott@imperial.ac.uk>
2017-11-05 09:54:25 +0000

    Added interfaces to nulike 1.0.6 and diver 1.0.4 	new file:   Backends/include/gambit/Backends/frontends/nulike_1_0_6.hpp 	new file:   Backends/src/frontends/nulike_1_0_6.cpp 	new file:   ScannerBit/include/gambit/ScannerBit/scanners/diver/1.0.4/diver.hpp 	new file:   ScannerBit/src/scanners/diver_1.0.4/diver.cpp 	modified:   cmake/backends.cmake 	modified:   cmake/scanners.cmake 	modified:   config/backend_locations.yaml.default 	modified:   config/scanner_locations.yaml.default

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -377,6 +377,26 @@ if(NOT ditched_${name}_${ver})
     INSTALL_COMMAND ""
   )
   add_extra_targets("backend" ${name} ${ver} ${dir} ${dl} distclean)
+endif()
+
+# Nulike
+set(name "nulike")
+set(ver "1.0.6")
+set(lib "libnulike")
+set(dl "https://www.hepforge.org/archive/${name}/${name}-${ver}.tar.gz")
+set(md5 "fc4c35dc867bb1213d80acd12e5c1169")
+set(dir "${PROJECT_SOURCE_DIR}/Backends/installed/${name}/${ver}")
+check_ditch_status(${name} ${ver})
+if(NOT ditched_${name}_${ver})
+  ExternalProject_Add(${name}_${ver}
+    DOWNLOAD_COMMAND ${DL_BACKEND} ${dl} ${md5} ${dir} ${name} ${ver}
+    SOURCE_DIR ${dir}
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} ${lib}.so FF=${CMAKE_Fortran_COMPILER} FOPT=${GAMBIT_Fortran_FLAGS} MODULE=${FMODULE}
+    INSTALL_COMMAND ""
+  )
+  add_extra_targets("backend" ${name} ${ver} ${dir} ${dl} distclean)
   set_as_default_version("backend" ${name} ${ver})
 endif()
 
