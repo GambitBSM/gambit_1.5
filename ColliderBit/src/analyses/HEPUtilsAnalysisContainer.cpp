21d0da2c6665831585871ac7a57dcb57f117f175
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2018-04-29 18:32:59 +0200

    Commented out debug flag and removed two analyses from the analysis container.
    	modified:   ColliderBit/src/ColliderBit_eventloop.cpp
    	modified:   ColliderBit/src/analyses/HEPUtilsAnalysisContainer.cpp

--- ColliderBit/src/analyses/HEPUtilsAnalysisContainer.cpp
+++ ColliderBit/src/analyses/HEPUtilsAnalysisContainer.cpp
@@ -20,11 +20,9 @@ namespace Gambit
       F(ATLAS_13TeV_0LEP_13invfb)                    \
       F(ATLAS_13TeV_0LEP_36invfb)                    \
       F(ATLAS_13TeV_0LEPStop_36invfb)                \
-      F(ATLAS_13TeV_1LEPStop_36invfb)                \
       F(ATLAS_13TeV_2LEPStop_36invfb)                \
       F(ATLAS_13TeV_MultiLEP_confnote_36invfb)       \
       F(ATLAS_13TeV_MultiLEP_36invfb)                \
-      F(ATLAS_13TeV_RJ3L_lowmass_36invfb)            \
       F(ATLAS_13TeV_2bMET_36invfb)                   \
       F(ATLAS_8TeV_0LEP_20invfb)                     \
       F(ATLAS_8TeV_0LEPStop_20invfb)                 \
