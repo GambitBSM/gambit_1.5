c6b1933a58e1046d6b9eb060e1548b59fd3a0d45
Andrew Fowlie <andrew.j.fowlie@googlemail.com>
2018-05-17 09:58:20 +1000

    added h -> chargino decays

--- DecayBit/include/gambit/DecayBit/MSSM_H.hpp
+++ DecayBit/include/gambit/DecayBit/MSSM_H.hpp
@@ -42,6 +42,38 @@ double lambda(double x, double y, double z) {
   return pow(1. - x / z - y / z, 2) - 4. * x * y / pow(z, 2);
 }
 
+double gamma_h_chi(std::array<double, 2> m,
+  double gL,
+  double mh = 125.,
+  double mw = 80.385,
+  double GF = 1.1663787e-5) {
+  /**
+     @brief Eq. 2.56 for charginos or neutralinos
+     @returns \f$\Gamma(h \to \chi_i \chi_j)\f$ in GeV
+     @param m Masses of final state particles
+     @param gL Coupling \f$g_L\f$ in Eq. 1.112
+     @param mh Lightest Higgs mass, \f$m_h\f$
+     @param mw W-boson mass, \f$M_W\f$
+     @param GF Fermi constant, \f$G_F\f$
+  */
+  const double eps_2 = 1.;
+  const double gR = gL * eps_2;
+
+  // Phase-space
+  double l = lambda(pow(m[0], 2), pow(m[1], 2), pow(mh, 2));
+  if (l <= 0.) {
+    return 0.;
+  }
+
+  // Eq. 2.56 without common factor (without f$\sin\theta_W\f$ which cancels)
+  const double gamma_no_prefactor = std::sqrt(l) * ((
+    pow(gL, 2) + pow(gR, 2)) * (1. - (pow(m[0], 2) + pow(m[1], 2)) / pow(mh, 2))
+    -4. * gL * gR * m[0] * m[1] / pow(mh, 2));
+
+  // Eq. 2.56 with common factor
+  return GF * pow(mw, 2) / (2. * M_SQRT2 * M_PI) * mh * gamma_no_prefactor;
+}
+
 double gamma_h_chi_0(int i,
   int j,
   std::array<double, 4> m,
@@ -53,50 +85,79 @@ double gamma_h_chi_0(int i,
   double sw2 = 0.22) {
   /**
      @brief Lightest Higgs boson decay to neutralinos at tree-level in GeV
-     
+
      @warning Arguments \f$i\f$ and \f$j\f$ are zero-based
      @warning Tree-level formula
-     
+
      @returns \f$\Gamma(h \to \chi_i \chi_j)\f$ in GeV
      @param i Neutralino \f$\chi_i\f$ in final state
      @param j Neutralino \f$\chi_j\f$ in final state
-     @param m Neutralino mass matrix with phases
+     @param m Neutralino mases with phases
      @param Z Real neutralino mixing matrix
      @param alpha \f$\alpha\f$, Higgs mixing angle
      @param mh Lightest Higgs mass, \f$m_h\f$
+     @param mw W-boson mass, \f$M_W\f$
      @param GF Fermi constant, \f$G_F\f$
      @param sw2 Weinberg angle, \f$\sin^2\theta_W\f$
-  */  
-  const double sw = std::sqrt(sw2);
-  const double cw = std::sqrt(1. - sw2);
-  const double tw = sw / cw;
+  */
+  // Weinberg angle
+  const double tw = std::sqrt(sw2) / std::sqrt(1. - sw2);
 
   // Eq. 1.113
   const double e2 = -std::sin(alpha);
   const double d2 = -std::cos(alpha);
-  const double eps_2 = 1.;
-
-  // Phase-space
-  double l = lambda(pow(m[i], 2), pow(m[j], 2), pow(mh, 2));
-  if (l <= 0.) {
-    return 0.;
-  }
 
   // Eq. 1.112 (without \f$\sin\theta_W\f$ which cancels)
   const double gL = 0.5 * (
     (Z[j][1] - tw * Z[j][0]) * (e2 * Z[i][2] + d2 * Z[i][3]) +
     (Z[i][1] - tw * Z[i][0]) * (e2 * Z[j][2] + d2 * Z[j][3]));
-  const double gR = eps_2 * gL;
 
-  // Eq. 2.56 without common factor (without f$\sin\theta_W\f$ which cancels)
-  const double delta = (i == j) ? 1. : 0.;
-  const double gamma_no_prefactor = std::sqrt(l) / (1. + delta) * ((
-    pow(gL, 2) + pow(gR, 2)) * (1. - (pow(m[i], 2) + pow(m[j], 2)) / pow(mh, 2))
-    -4. * gL * gR * m[i] * m[j] / pow(mh, 2));
-
-  // Eq. 2.56 with common factor
-  return GF * pow(mw, 2) / (2. * M_SQRT2 * M_PI) * mh * gamma_no_prefactor;
+  std::array<double, 2> mf{{m[i], m[j]}};
+  double gamma = gamma_h_chi(mf, gL, mh, mw, GF);
+  if (i == j) {
+    gamma *= 0.5;
+  }
+  return gamma;
 }
+
+double gamma_h_chi_pm(int i,
+  int j,
+  std::array<double, 2> m,
+  std::array<std::array<double, 2>, 2> U,
+  std::array<std::array<double, 2>, 2> V,
+  double alpha,
+  double mh = 125.,
+  double mw = 80.385,
+  double GF = 1.1663787e-5) {
+  /**
+     @brief Lightest Higgs boson decay to charginos at tree-level in GeV
+
+     @warning Arguments \f$i\f$ and \f$j\f$ are zero-based
+     @warning Tree-level formula
+
+     @returns \f$\Gamma(h \to \chi^-_i \chi^+_j)\f$ in GeV
+     @param i Negativ chargino \f$\chi^-_i\f$ in final state
+     @param j Positive chargino \f$\chi^+_j\f$ in final state
+     @param m Chargino masses
+     @param U Real chargino mixing matrix
+     @param V Real chargino mixing matrix
+     @param alpha \f$\alpha\f$, Higgs mixing angle
+     @param mh Lightest Higgs mass, \f$m_h\f$
+     @param mw W-boson mass, \f$M_W\f$
+     @param GF Fermi constant, \f$G_F\f$
+  */
+  // Eq. 1.113
+  const double e2 = -std::sin(alpha);
+  const double d2 = -std::cos(alpha);
+
+  // Eq. 1.112 (without \f$\sin\theta_W\f$ which cancels)
+  const double gL = M_SQRT1_2 *
+    (e2 * V[j][0] * U[i][1] - d2 * V[j][1] * U[i][0]);
+
+  std::array<double, 2> mf{{m[i], m[j]}};
+  return gamma_h_chi(mf, gL, mh, mw, GF);
+}
+
 }  // namespace MSSM_H
 
 #endif  //  DECAYBIT_INCLUDE_GAMBIT_DECAYBIT_MSSM_H_HPP_
