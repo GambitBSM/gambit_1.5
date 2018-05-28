8c5f4ab44708899868e70c21b4024355e869199d
Andrew Fowlie <andrew.j.fowlie@googlemail.com>
2018-05-28 15:34:06 +1000

    add alpha thomson to numerical constants

--- Utils/include/gambit/Utils/numerical_constants.hpp
+++ Utils/include/gambit/Utils/numerical_constants.hpp
@@ -39,6 +39,9 @@ namespace Gambit
   const double m_neutron_amu = 1.0086649156;                    // neutron mass (amu)
   const double m_proton = m_proton_amu * atomic_mass_unit;      // proton mass (GeV/c^2)
   const double m_neutron = m_neutron_amu * atomic_mass_unit;    // neutron mass (GeV/c^2)
+  
+  /* @brief Thomson limit of \f$\alpha_e\f$*/
+  const double alpha_e_thomson_limit = 0.00775531;
 
   static const struct Mesons_masses
   {
