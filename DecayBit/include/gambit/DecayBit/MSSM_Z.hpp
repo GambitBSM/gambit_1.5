5d038b5865ddfd4de910a039daecbdf2fa5bb393
Andrew Fowlie <andrew.j.fowlie@googlemail.com>
2018-05-28 10:53:13 +1000

    add gambit/decaybit namespaces + indent them all

--- DecayBit/include/gambit/DecayBit/MSSM_Z.hpp
+++ DecayBit/include/gambit/DecayBit/MSSM_Z.hpp
@@ -22,49 +22,53 @@
 #include <array>
 #include <cmath>
 
-namespace MSSM_Z {
+namespace Gambit {
+  namespace DecayBit {
+    namespace MSSM_Z {
 
-double gamma_chi_0(int i,
-  int j,
-  std::array<double, 4> m,
-  std::array<std::array<double, 4>, 4> Z,
-  double g2 = 0.652,
-  double MZ = 91.,
-  double sw2 = 0.22) {
-  /**
-     @brief \f$\Gamma(Z \to \chi_i \chi_j)\f$ in GeV
+      double gamma_chi_0(int i,
+        int j,
+        std::array<double, 4> m,
+        std::array<std::array<double, 4>, 4> Z,
+        double g2 = 0.652,
+        double MZ = 91.,
+        double sw2 = 0.22) {
+        /**
+           @brief \f$\Gamma(Z \to \chi_i \chi_j)\f$ in GeV
 
-     @warning Tree-level formula
+           @warning Tree-level formula
 
-     @returns \f$\Gamma(h \to \chi_i \chi_j)\f$ in GeV
-     @param m Neutralino masses, \f$m_{\chi_i}\f$, without phases
-     @param Z Real neutralino mixing matrix
-     @param g2 \f$g_2\f$
-     @param MZ \f$M_Z\f$, Z-boson mass
-     @param sw2 Weinberg angle, \f$\sin^2\theta_W\f$
-  */
+           @returns \f$\Gamma(h \to \chi_i \chi_j)\f$ in GeV
+           @param m Neutralino masses, \f$m_{\chi_i}\f$, without phases
+           @param Z Real neutralino mixing matrix
+           @param g2 \f$g_2\f$
+           @param MZ \f$M_Z\f$, Z-boson mass
+           @param sw2 Weinberg angle, \f$\sin^2\theta_W\f$
+        */
 
-  const double p2 = (pow(MZ, 2) - pow(m[i] + m[j], 2)) *
-    (pow(MZ, 2) - pow(m[i] - m[j], 2)) / (4. * pow(MZ, 2));
-  const double ei = (pow(MZ, 2) - pow(m[j], 2) + pow(m[i], 2)) / (2. * MZ);
-  const double ej = (pow(MZ, 2) - pow(m[i], 2) + pow(m[j], 2)) / (2. * MZ);
+        const double p2 = (pow(MZ, 2) - pow(m[i] + m[j], 2)) *
+          (pow(MZ, 2) - pow(m[i] - m[j], 2)) / (4. * pow(MZ, 2));
+        const double ei = (pow(MZ, 2) - pow(m[j], 2) + pow(m[i], 2)) / (2. * MZ);
+        const double ej = (pow(MZ, 2) - pow(m[i], 2) + pow(m[j], 2)) / (2. * MZ);
 
-  if (!(p2 > 0. && ei > 0. && ej > 0.)) {
-    return 0.;
-  }
+        if (!(p2 > 0. && ei > 0. && ej > 0.)) {
+          return 0.;
+        }
 
-  const double cw = std::sqrt(1. - sw2);
+        const double cw = std::sqrt(1. - sw2);
 
-  const double gz = g2 / (2. * cw) * (Z[i][2] * Z[j][2] - Z[i][3] * Z[j][3]);
-  double gamma = sqrt(p2) / (2. * M_PI * pow(MZ, 2)) * pow(gz, 2) *
-    (ei * ej + p2 / 3. - m[i] * m[j]);
+        const double gz = g2 / (2. * cw) * (Z[i][2] * Z[j][2] - Z[i][3] * Z[j][3]);
+        double gamma = sqrt(p2) / (2. * M_PI * pow(MZ, 2)) * pow(gz, 2) *
+          (ei * ej + p2 / 3. - m[i] * m[j]);
 
-  if (i == j) {
-    gamma *= 0.5;
-  }
+        if (i == j) {
+          gamma *= 0.5;
+        }
 
-  return gamma;
-}
-}  // namespace MSSM_Z
+        return gamma;
+      }
+    }  // namespace MSSM_Z
+  }  // namespace DecayBit
+}  // namespace Gambit
 
 #endif  //  DECAYBIT_INCLUDE_GAMBIT_DECAYBIT_MSSM_Z_HPP_
