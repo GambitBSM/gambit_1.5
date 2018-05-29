953ffef49146edb7dcf0ab9b8ef663b90bb7d0b0
Christoph Weniger <c.weniger@uva.nl>
2018-05-29 16:33:27 +0200

    Add differential active neutrino mass model.

---
+++ Models/src/models/StandardModel_mNudiff.cpp
@@ -0,0 +1,54 @@
+//   GAMBIT: Global and Modular BSM Inference Tool
+//   *********************************************
+///  \file
+///
+///  RH Neutrino Model with differential masses
+///
+///  *********************************************
+///
+///  Authors (add name and date if you modify):
+///   
+///  \author Tomas Gonzao  
+///          (t.e.gonzalo@fys.uio.no)
+///  \date 2017 December
+///
+///  *********************************************
+
+#include "gambit/Models/model_macros.hpp"
+#include "gambit/Models/model_helpers.hpp"
+#include "gambit/Logs/logger.hpp"
+
+#include "gambit/Models/models/StandardModel_mNudiff.hpp"
+
+
+#define MODEL StandardModel_mNudiff
+  void MODEL_NAMESPACE::StandardModel_SLHA2_to_StandardModel_mNudiff (const ModelParameters &myP, ModelParameters &targetP)
+  {
+
+     logger()<<"Running interpret_as_parent calculations for StandardModel_mNudiff --> StandardModel_SLHA2."<<LogTags::info<<EOM;
+     
+     // Send all parameter values upstream to matching parameters in parent.
+     // Ignore that some parameters don't exist in the parent, as these are set below.
+     targetP.setValues(myP,false);
+
+     // M2
+
+     if (myP["dmNu3l"] > 0.)  // normal hierarchy, l = 2
+     {
+       targetP.setValue("mNu1", myP["mNu_light"]);
+       targetP.setValue("mNu2",
+           pow(myP["mNu_light"]*myP["mNu_light"]+myP["dmNu21"], 0.5));
+       targetP.setValue("mNu3",
+           pow(myP["mNu_light"]*myP["mNu_light"]+myP["dmNu3l"]+myP["dmNu21"], 0.5));
+     }
+     else // inverted hierarchy, l = 1
+     {
+       targetP.setValue("mNu3", myP["mNu_light"]);
+       targetP.setValue("mNu1",
+           pow(myP["mNu_light"]*myP["mNu_light"]-myP["dmNu3l"], 0.5));
+       targetP.setValue("mNu2",
+           pow(myP["mNu_light"]*myP["mNu_light"]-myP["dmNu3l"]+myP["dmNu21"], 0.5));
+     }
+  }
+
+#undef MODEL
