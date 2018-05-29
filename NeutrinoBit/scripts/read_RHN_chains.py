71d3d30b67eb91219834cc8b80c8ddb87790f5df
Christoph Weniger <c.weniger@uva.nl>
2018-05-29 20:39:17 +0200

    Updates scripts

--- NeutrinoBit/scripts/read_RHN_chains.py
+++ NeutrinoBit/scripts/read_RHN_chains.py
@@ -25,42 +25,46 @@ class RHN_Chain(object):
                 print key
             quit()
 
+        valid = np.array(group['LogLike_isvalid'], dtype = 'bool')
+        def get_data(tag):
+            return np.array(group[tag])[valid]
+
         if MODEL == 'diff':
-            self.M1 = np.array(group['#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::M_1'])
-            self.dM2 = np.array(group['#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::delta_M_2'])
-            self.dM3 = np.array(group['#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::delta_M_3'])
-            self.ReOmega12 = np.array(group['#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ReOm12'])
-            self.ReOmega13 = np.array(group['#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ReOm13'])
-            self.ReOmega23 = np.array(group['#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ReOm23'])
-            self.ImOmega12 = np.array(group['#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ImOm12'])
-            self.ImOmega13 = np.array(group['#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ImOm13'])
-            self.ImOmega23 = np.array(group['#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ImOm23'])
+            self.M1 = get_data('#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::M_1')
+            self.dM2 = get_data('#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::delta_M_2')
+            self.dM3 = get_data('#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::delta_M_3')
+            self.ReOmega12 = get_data('#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ReOm12')
+            self.ReOmega13 = get_data('#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ReOm13')
+            self.ReOmega23 = get_data('#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ReOm23')
+            self.ImOmega12 = get_data('#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ImOm12')
+            self.ImOmega13 = get_data('#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ImOm13')
+            self.ImOmega23 = get_data('#RightHandedNeutrinos_diff_parameters @RightHandedNeutrinos_diff::primary_parameters::ImOm23')
             self.M2 = self.M1 + self.dM2
             self.M3 = self.M1 + self.dM3
         elif MODEL == 'full':
-            self.M1 = np.array(group['#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::M_1'])
-            self.M2 = np.array(group['#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::M_2'])
-            self.M3 = np.array(group['#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::M_3'])
-            self.ReOmega12 = np.array(group['#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ReOm12'])
-            self.ReOmega13 = np.array(group['#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ReOm13'])
-            self.ReOmega23 = np.array(group['#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ReOm23'])
-            self.ImOmega12 = np.array(group['#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ImOm12'])
-            self.ImOmega13 = np.array(group['#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ImOm13'])
-            self.ImOmega23 = np.array(group['#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ImOm23'])
+            self.M1 = get_data('#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::M_1')
+            self.M2 = get_data('#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::M_2')
+            self.M3 = get_data('#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::M_3')
+            self.ReOmega12 = get_data('#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ReOm12')
+            self.ReOmega13 = get_data('#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ReOm13')
+            self.ReOmega23 = get_data('#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ReOm23')
+            self.ImOmega12 = get_data('#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ImOm12')
+            self.ImOmega13 = get_data('#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ImOm13')
+            self.ImOmega23 = get_data('#RightHandedNeutrinos_parameters @RightHandedNeutrinos::primary_parameters::ImOm23')
             self.dM2 = self.M2 - self.M1
             self.dM3 = self.M3 - self.M1
         else:
             raise KeyError("Model unknown")
 
-        self.Ue1 = np.array(group['#Ue1 @NeutrinoBit::Ue1'])
-        self.Ue2 = np.array(group['#Ue2 @NeutrinoBit::Ue2'])
-        self.Ue3 = np.array(group['#Ue3 @NeutrinoBit::Ue3'])
-        self.Um1 = np.array(group['#Um1 @NeutrinoBit::Um1'])
-        self.Um2 = np.array(group['#Um2 @NeutrinoBit::Um2'])
-        self.Um3 = np.array(group['#Um3 @NeutrinoBit::Um3'])
-        self.Ut1 = np.array(group['#Ut1 @NeutrinoBit::Ut1'])
-        self.Ut2 = np.array(group['#Ut2 @NeutrinoBit::Ut2'])
-        self.Ut3 = np.array(group['#Ut3 @NeutrinoBit::Ut3'])
+        self.Ue1 = get_data('#Ue1 @NeutrinoBit::Ue1')
+        self.Ue2 = get_data('#Ue2 @NeutrinoBit::Ue2')
+        self.Ue3 = get_data('#Ue3 @NeutrinoBit::Ue3')
+        self.Um1 = get_data('#Um1 @NeutrinoBit::Um1')
+        self.Um2 = get_data('#Um2 @NeutrinoBit::Um2')
+        self.Um3 = get_data('#Um3 @NeutrinoBit::Um3')
+        self.Ut1 = get_data('#Ut1 @NeutrinoBit::Ut1')
+        self.Ut2 = get_data('#Ut2 @NeutrinoBit::Ut2')
+        self.Ut3 = get_data('#Ut3 @NeutrinoBit::Ut3')
 
         lnL_tags = [
             '#deltaCP_lnL @NeutrinoBit::deltaCP_lnL',
@@ -111,35 +115,33 @@ class RHN_Chain(object):
         self.lnL_partial = {}
         for tag, name in zip(lnL_tags, lnL_names):
             try:
-                self.lnL_partial[name] = np.array(group[tag])
+                self.lnL_partial[name] = get_data(tag)
             except KeyError:
                 print "WARNING: Did not find", name
                 pass
 
-        self.lnL = np.array(group['LogLike'])
+        self.lnL = get_data('LogLike')
 
-        self.valid = np.array(group['LogLike_isvalid'], dtype = 'bool')
-
-        self.ordering = np.array(group['#ordering @NeutrinoBit::ordering'])
+        self.ordering = get_data('#ordering @NeutrinoBit::ordering')
 
         self.U1 = self.Ue1 + self.Um1 + self.Ut1
         self.U2 = self.Ue2 + self.Um2 + self.Ut2
         self.U3 = self.Ue3 + self.Um3 + self.Ut3
         self.U = self.U1 + self.U2 + self.U3
 
-        self.mNu1 = np.array(group['#StandardModel_SLHA2_parameters @StandardModel_SLHA2::primary_parameters::mNu1'])
-        self.mNu2 = np.array(group['#StandardModel_SLHA2_parameters @StandardModel_SLHA2::primary_parameters::mNu2'])
-        self.mNu3 = np.array(group['#StandardModel_SLHA2_parameters @StandardModel_SLHA2::primary_parameters::mNu3'])
+        self.mNu1 = get_data('#StandardModel_SLHA2_parameters @StandardModel_SLHA2::primary_parameters::mNu1')
+        self.mNu2 = get_data('#StandardModel_SLHA2_parameters @StandardModel_SLHA2::primary_parameters::mNu2')
+        self.mNu3 = get_data('#StandardModel_SLHA2_parameters @StandardModel_SLHA2::primary_parameters::mNu3')
         self.mMin = np.minimum(np.minimum(self.mNu1, self.mNu2), self.mNu3)
 
-        self.md21 = np.array(group['#md21 @NeutrinoBit::md21'])
-        self.md31 = np.array(group['#md31 @NeutrinoBit::md31'])
-        self.md32 = np.array(group['#md32 @NeutrinoBit::md32'])
+        self.md21 = get_data('#md21 @NeutrinoBit::md21')
+        self.md31 = get_data('#md31 @NeutrinoBit::md31')
+        self.md32 = get_data('#md32 @NeutrinoBit::md32')
 
-        self.mask2sigma = self.valid & (self.lnL > self.lnL.max() - 0.5*4)
+        mask2sigma = self.lnL > self.lnL.max() - 0.5*4
 
-        print "Number of points: %i"%len(self.valid)
-        print "Number of valid points: %i"%self.valid.sum()
-        print "Number of valid points within 2 sigma: %i"%self.mask2sigma.sum()
-        print "Number of valid points within 2 sigma (fraction): %.1e"%(self.mask2sigma.sum()/len(self.valid))
+        print "Number of points: %i"%len(valid)
+        print "Number of valid points: %i"%valid.sum()
+        print "Number of valid points within 2 sigma: %i"%mask2sigma.sum()
+        print "Number of valid points within 2 sigma (fraction): %.1e"%(mask2sigma.sum()/len(valid))
 
