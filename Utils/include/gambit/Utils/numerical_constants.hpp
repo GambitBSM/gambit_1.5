5dc1d9bf4ccf1a8e1db313e020df0b07de170f29
Andrew Fowlie <andrew.j.fowlie@googlemail.com>
2018-05-29 12:05:01 +1000

    add URL to docstring

--- Utils/include/gambit/Utils/numerical_constants.hpp
+++ Utils/include/gambit/Utils/numerical_constants.hpp
@@ -40,8 +40,11 @@ namespace Gambit
   const double m_proton = m_proton_amu * atomic_mass_unit;      // proton mass (GeV/c^2)
   const double m_neutron = m_neutron_amu * atomic_mass_unit;    // neutron mass (GeV/c^2)
   
-  /* @brief Thomson limit of \f$\alpha_e\f$*/
-  const double alpha_e_thomson_limit = 0.0072973525664; // http://pdg.lbl.gov/2017/reviews/rpp2017-rev-phys-constants.pdf
+  /**
+     @brief Thomson limit of \f$\alpha_e\f$ from
+     <a href="http://pdg.lbl.gov/2017/reviews/rpp2017-rev-phys-constants.pdf">PDG 2017</a>
+  */
+  const double alpha_e_thomson_limit = 0.0072973525664;
 
   static const struct Mesons_masses
   {
