0b2d762d676f2fc689604b3a13fffbd3703fb867
Tomas Gonzalo <t.e.gonzalo@fys.uio.no>
2018-07-27 11:37:00 +0200

    More changes on coupling slide and yaml files

--- NeutrinoBit/src/RightHandedNeutrinos.cpp
+++ NeutrinoBit/src/RightHandedNeutrinos.cpp
@@ -2349,42 +2349,14 @@ namespace Gambit
       {
 	if (runOptions->getValueOrDef<bool>(true, "Ut_lim"))
 	{
-	  if (runOptions->getValueOrDef<bool>(true, "all_Ut"))
-	  {	  
-            if( ((M[0] > 60) and (Ut1 > 1e-4)) or ((M[1] > 60) and (Ut2 > 1e-4)) or ((M[2] > 60) and (Ut3 > 1e-4)) )
-            {
-              std::ostringstream msg;
-              msg << "Tau coupling restricted; point invalidated.";
-              logger() << msg.str() << EOM;
-              invalid_point().raise(msg.str());
-              return ;
-            }
-	  }
-          else
-	  {
-	    if (flavour == 3)
-	    {
-              if( ((M[1] > 60) and (Ut2 > 1e-4)) or ((M[2] > 60) and (Ut3 > 1e-4)) )
-              {
-                std::ostringstream msg;
-                msg << "Tau coupling restricted; point invalidated.";
-                logger() << msg.str() << EOM;
-                invalid_point().raise(msg.str());
-                return ;
-              }
-            }
-	    else
-	    {	
-              if( ((M[0] > 60) and (Ut1 > 1e-4)) or ((M[1] > 60) and (Ut2 > 1e-4)) or ((M[2] > 60) and (Ut3 > 1e-4)) )
-              {
-                std::ostringstream msg;
-                msg << "Tau coupling restricted; point invalidated.";
-                logger() << msg.str() << EOM;
-                invalid_point().raise(msg.str());
-                return ;
-              }
-	    }
-	  }
+          if( ((M[0] > 60) and (Ut1 > 1e-4)) or ((M[1] > 60) and (Ut2 > 1e-4)) or ((M[2] > 60) and (Ut3 > 1e-4)) )
+          {
+            std::ostringstream msg;
+            msg << "Tau coupling restricted; point invalidated.";
+            logger() << msg.str() << EOM;
+            invalid_point().raise(msg.str());
+            return ;
+          }
         }	  
         double U = (Dep::SeesawI_Theta->cwiseAbs2())(flavour-1,I-1);
         lnL = slope*log10(std::min(U, 1.)) + mslope*log10(M[I-1]);
