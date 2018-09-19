eda0d3979e975106994dcc0adc3d6f26b36edda3
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2018-09-19 17:59:46 +0200

    Revert "Changed the printer BUFFERLENGTH to 1 for the MSSMEW post-processing. This only makes sense for these kinds of tasks, where"
    This reverts commit 5c9b02e55e58c9d51b77ed2c425ba79f79e7193b.
    
    	modified:   Printers/include/gambit/Printers/printers/hdf5printer.hpp

--- Printers/include/gambit/Printers/printers/hdf5printer.hpp
+++ Printers/include/gambit/Printers/printers/hdf5printer.hpp
@@ -57,8 +57,7 @@ namespace Gambit
   {
 
     // Parameter controlling the length of all the standard buffers
-    // Temp hack: Setting this to 1 for the MSSMEW scans. 
-    static const std::size_t BUFFERLENGTH = 1; // 100; // Change to 10000 or something. Currently cannot change this dynamically though, sorry.
+    static const std::size_t BUFFERLENGTH = 100; // Change to 10000 or something. Currently cannot change this dynamically though, sorry.
     /// Max number of PPIDpairs to be tracked
     /// i.e. chunks of RA output longer than this can potentially contain multiple writes to the same point.
     /// It is up to the combine script to apply the last scheduled write preferentially.
