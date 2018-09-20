afc45aa1b97d20cc00b449e3419bc96a7b3ebf62
Pat Scott <p.scott@imperial.ac.uk>
2018-09-20 23:57:26 +0200

    new file:   Models/include/gambit/Models/models/ScalarSingletDM_Z3_running.hpp 	renamed:    contrib/MassSpectra/flexiblesusy/models/ScalarSingletDM_Z2/run_SingletDM.m -> contrib/MassSpectra/flexiblesusy/models/ScalarSingletDM_Z2/run_ScalarSingletDM_Z2.m

---
+++ Models/include/gambit/Models/models/ScalarSingletDM_Z3_running.hpp
@@ -0,0 +1,29 @@
+//  GAMBIT: Global and Modular BSM Inference Tool
+//  *********************************************
+///
+///  Z3 scalar singlet dark matter with running mass
+///  and quartic coupling
+///
+///  *********************************************
+///
+///  Authors
+///  =======
+///
+///  \author James McKay
+///  \date 2015 September
+///
+///  \author Pat Scott
+///          (p.scott@imperial.ac.uk)
+///  \date 2018 Sep
+///
+///  *********************************************
+
+#ifndef __ScalarSingletDM_Z3_hpp__
+#define __ScalarSingletDM_Z3_hpp__
+
+#define MODEL ScalarSingletDM_Z3_running
+  START_MODEL
+  DEFINEPARS(mS, lambda_hS, lambda_S, mu3)
+#undef MODEL
+
+#endif
