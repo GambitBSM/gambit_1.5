c5ae1e702775ce672760df7765a185d63c646b90
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2018-09-27 16:03:19 +0200

    Further tweaks to cmake setup for RestFrames
    	modified:   cmake/FindRestFrames.cmake
    	modified:   cmake/optional.cmake

--- cmake/FindRestFrames.cmake
+++ cmake/FindRestFrames.cmake
@@ -7,11 +7,9 @@
 # TODO: Look for RestFrames in other places than just contrib/
 set(ver "1.0.1")
 set(dir "${PROJECT_SOURCE_DIR}/contrib/RestFrames-${ver}")
-unset(RestFrames_1.0.1_LIBRARY CACHE)
-find_library(RestFrames_${ver}_LIBRARY RestFrames 
-  ${dir}/lib/
-)
-if(RestFrames_1.0.1_LIBRARY STREQUAL "RestFrames_1.0.1_LIBRARY-NOTFOUND")
+unset(RestFrames_LIBRARY CACHE)
+find_library(RestFrames_LIBRARY RestFrames ${dir}/lib/)
+if(RestFrames_LIBRARY STREQUAL "RestFrames_LIBRARY-NOTFOUND")
   set(RestFrames_FOUND FALSE)
 else()
   set(RestFrames_FOUND TRUE)
