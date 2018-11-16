498a4923ac9b3d68672d14726ef885b556cd3849
Pat Scott <p.scott@imperial.ac.uk>
2018-11-16 20:03:49 +0000

    More fixes to compilation without pybind11 	modified:   Backends/include/gambit/Backends/python_function.hpp 	modified:   Backends/include/gambit/Backends/python_macros.hpp 	modified:   Backends/src/frontends/LibSecond_1_1.cpp 	modified:   Backends/src/frontends/LibThird_1_2.cpp

--- Backends/include/gambit/Backends/python_function.hpp
+++ Backends/include/gambit/Backends/python_function.hpp
@@ -18,11 +18,13 @@
 #ifndef __python_function_hpp__
 #define __python_function_hpp__
 
-#include <pybind11/pybind11.h>
+#ifdef HAVE_PYBIND11
+  #include <pybind11/pybind11.h>
+  #include "gambit/Backends/python_helpers.hpp"
+#endif
 
 #include "gambit/Elements/ini_catch.hpp"
 #include "gambit/Backends/backend_singleton.hpp"
-#include "gambit/Backends/python_helpers.hpp"
 #include "gambit/cmake/cmake_variables.hpp"
 
 namespace Gambit
@@ -37,67 +39,81 @@ namespace Gambit
 
       private:
 
-        /// Wrapper to the function
-        pybind11::object func;
+        #ifdef HAVE_PYBIND11
 
-        /// Indication of whether or not the function has been successfully loaded
-        bool handle_works;
+          /// Wrapper to the function
+          pybind11::object func;
+
+          /// Indication of whether or not the function has been successfully loaded
+          bool handle_works;
+
+        #endif
 
       public:
 
         /// Constructor
-        python_function(const str& be, const str& ver, const str& symbol) : handle_works(false)
+        python_function(const str& be, const str& ver, const str& symbol)
+        #ifdef HAVE_PYBIND11
+          : handle_works(false)
+        #endif
         {
-          try
-          {
-            // Extract the backend module pointer from the backendInfo object
-            pybind11::object* mod;
-            if (backendInfo().works.at(be+ver))
-            {
-              mod = backendInfo().loaded_python_backends.at(be+ver);
-            }
-            else
-            {
-              mod = (pybind11::module*)0;
-              return;
-            }
-
-            // Work out if this is a function in the main part of the package, or in a sub-module
-            sspair module_and_name = split_qualified_python_name(symbol, backendInfo().lib_name(be, ver));
-
-            // Extract the function from the module
+          #ifdef HAVE_PYBIND11
             try
             {
-              if (module_and_name.first.empty())
+              // Extract the backend module pointer from the backendInfo object
+              pybind11::object* mod;
+              if (backendInfo().works.at(be+ver))
               {
-                func = mod->attr(symbol.c_str());
+                mod = backendInfo().loaded_python_backends.at(be+ver);
               }
               else
               {
-                pybind11::module sub_module = pybind11::module::import(module_and_name.first.c_str());
-                func = sub_module.attr(module_and_name.second.c_str());
+                mod = (pybind11::module*)0;
+                return;
               }
-              handle_works = true;
+
+              // Work out if this is a function in the main part of the package, or in a sub-module
+              sspair module_and_name = split_qualified_python_name(symbol, backendInfo().lib_name(be, ver));
+
+              // Extract the function from the module
+              try
+              {
+                if (module_and_name.first.empty())
+                {
+                  func = mod->attr(symbol.c_str());
+                }
+                else
+                {
+                  pybind11::module sub_module = pybind11::module::import(module_and_name.first.c_str());
+                  func = sub_module.attr(module_and_name.second.c_str());
+                }
+                handle_works = true;
+              }
+              catch (std::exception& e)
+              {
+                std::ostringstream err;
+                err << "Failed to retrieve handle to function " << symbol << " from Python module for " << be+ver << endl
+                    << "The backend function from this symbol will be disabled (i.e. get status = -2)." << endl
+                    << "Python error was: " << e.what() << endl;
+                backend_warning().raise(LOCAL_INFO, err.str());
+                backendInfo().dlerrors[be+ver] = symbol;
+              }
             }
-            catch (std::exception& e)
-            {
-              std::ostringstream err;
-              err << "Failed to retrieve handle to function " << symbol << " from Python module for " << be+ver << endl
-                  << "The backend function from this symbol will be disabled (i.e. get status = -2)." << endl
-                  << "Python error was: " << e.what() << endl;
-              backend_warning().raise(LOCAL_INFO, err.str());
-              backendInfo().dlerrors[be+ver] = symbol;
-            }
-          }
-          catch (std::exception& e) { ini_catch(e); }
+            catch (std::exception& e) { ini_catch(e); }
+          #endif
         }
 
         /// Operation (execute function and return value)
         TYPE operator()(ARGS&&... args)
         {
-          if (not handle_works) backend_error().raise(LOCAL_INFO, "Attempted to call a Python backend function that was not successfully loaded.");
-          pybind11::object result = func(std::forward<ARGS>(args)...);
-          return return_cast<TYPE>(result);
+          #ifdef HAVE_PYBIND11
+            if (not handle_works) backend_error().raise(LOCAL_INFO, "Attempted to call a Python backend function that was not successfully loaded.");
+            pybind11::object result = func(std::forward<ARGS>(args)...);
+            return return_cast<TYPE>(result);
+          #else
+            backend_error().raise(LOCAL_INFO, "Attempted to call a python function from C++ without pybind11.");
+            return TYPE();
+          #endif
         }
 
     };
@@ -106,4 +122,4 @@ namespace Gambit
 
 }
 
-#endif /* __function_hpp__ */
+#endif /* __python_function_hpp__ */
