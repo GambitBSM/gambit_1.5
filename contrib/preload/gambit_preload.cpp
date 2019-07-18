20a1813ce68e6c8143ff8d4efab22b1851c6ce5f
Pat Scott <p.scott@imperial.ac.uk>
2019-07-18 15:53:58 +0100

    Fixed treatment of patch name in preload message 	modified:   contrib/preload/gambit_preload.cpp

--- contrib/preload/gambit_preload.cpp
+++ contrib/preload/gambit_preload.cpp
@@ -12,7 +12,7 @@
 ///
 ///  \author Pat Scott
 ///          p.scott@imperial.ac.uk
-///  \date 2019 June
+///  \date 2019 June, July
 ///
 ///  *********************************************
 
@@ -23,12 +23,13 @@
 #include "gambit/cmake/cmake_variables.hpp"
 #include "gambit/Utils/stringify.hpp"
 
+
 // Initializer; runs as soon as this library is loaded.
 __attribute__((constructor))
 static void initializer()
 {
   printf("%s", "\n\x1b[1;33mGAMBIT " STRINGIFY(GAMBIT_VERSION_MAJOR) "." STRINGIFY(GAMBIT_VERSION_MINOR) "." STRINGIFY(GAMBIT_VERSION_REVISION));
-  if (GAMBIT_VERSION_PATCH != "") printf("%s", "-" STRINGIFY(GAMBIT_VERSION_PATCH));
+  if (strcmp(GAMBIT_VERSION_PATCH, "") != 0) printf("%s", "-" GAMBIT_VERSION_PATCH);
   printf("\nhttp://gambit.hepforge.org\n\n\x1b[0m");
   #ifndef EXCLUDE_RESTFRAMES
     const char* oldenv = getenv("CPLUS_INCLUDE_PATH");
