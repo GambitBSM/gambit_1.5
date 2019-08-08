6196dcd6dcc44d71fa29a2950fa7af0014c32843
Pat Scott <p.scott@imperial.ac.uk>
2019-08-08 09:28:25 +0100

    Updated tarball info for 1.4.0 	modified:   cmake/tarball_info.cmake

--- cmake/tarball_info.cmake
+++ cmake/tarball_info.cmake
@@ -1,10 +1,10 @@
 #*** GAMBIT ***********************
-# This file automatically generated 
-# by utlities.cmake. Do not modify. 
+# This file automatically generated
+# by utlities.cmake. Do not modify.
 #**********************************
 
 set(GAMBIT_VERSION_MAJOR 1)
-set(GAMBIT_VERSION_MINOR 3)
-set(GAMBIT_VERSION_REVISION 1)
+set(GAMBIT_VERSION_MINOR 4)
+set(GAMBIT_VERSION_REVISION 0)
 set(GAMBIT_VERSION_PATCH )
-set(GAMBIT_VERSION_FULL 1.3.1)
+set(GAMBIT_VERSION_FULL 1.4.0)
