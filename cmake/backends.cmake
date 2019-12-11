904f82a6e13346b4714da91d9b5122d48b36b9af
Patrick Stöcker <patrick.stoecker@rwth-aachen.de>
2019-12-11 18:29:35 +0100

    Allow for multiple MontePythonLike versions
     * As usual for Python-backends, the backend-file needs to dressed with its safe-version.
     * Also adjust hardcoded version number in logger message.
    
    	modified:       Backends/src/frontends/MontePythonLike_3_1_0.cpp
    	modified:       cmake/backends.cmake
    	modified:       config/backend_locations.yaml.default

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -546,6 +546,7 @@ endif()
 # MontePythonLike
 set(name "montepythonlike")
 set(ver "3.1.0")
+set(sfver "3_1_0")
 set(dl "null")
 set(dir "${PROJECT_SOURCE_DIR}/Backends/installed/${name}/${ver}")
 set(patchdir "${PROJECT_SOURCE_DIR}/Backends/patches/${name}/${ver}/")
@@ -557,8 +558,9 @@ if(NOT ditched_${name}_${ver})
     GIT_TAG 3.1.0
     SOURCE_DIR ${dir}
     BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy ${patchdir}/MontePythonLike.py ${dir}/montepython/MontePythonLike.py
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy ${patchdir}/MontePythonLike.py ${dir}/montepython/MontePythonLike_${sfver}.py
     COMMAND ${CMAKE_COMMAND} -E copy ${patchdir}/MPLike_patch_script.py ${dir}/montepython/MPLike_patch_script.py
+    COMMAND sed -i "s#from MontePythonLike import#from MontePythonLike_${sfver} import#g" ${dir}/montepython/MPLike_patch_script.py
     COMMAND ${CMAKE_COMMAND} -E copy ${patchdir}/sdss_lrgDR7_fiducialmodel.dat ${dir}/data/sdss_lrgDR7/sdss_lrgDR7_fiducialmodel.dat
     BUILD_COMMAND ""
     INSTALL_COMMAND python ${dir}/montepython/MPLike_patch_script.py
