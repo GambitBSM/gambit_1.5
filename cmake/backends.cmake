9c7ab16209470de8780327013d317c26efe99fe7
janinarenk <janina.renk@fysik.su.se>
2020-02-04 11:07:12 +1000

    Removed 'GIT_CONFIG advice.detachedHead=false' command for MontePython & DarkAges backends to avoid having to add a dependency on a higher cmake version
    	modified:   cmake/backends.cmake

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -555,7 +555,6 @@ check_ditch_status(${name} ${ver} ${dir})
 if(NOT ditched_${name}_${ver})
   ExternalProject_Add(${name}_${ver}
     GIT_REPOSITORY https://github.com/brinckmann/montepython_public.git
-    #GIT_CONFIG advice.detachedHead=false
     GIT_TAG 3.1.0
     SOURCE_DIR ${dir}
     BUILD_IN_SOURCE 1
@@ -1304,7 +1303,6 @@ check_ditch_status(${name} ${ver} ${dir})
 if(NOT ditched_${name}_${ver})
   ExternalProject_Add(${name}_${ver}
     GIT_REPOSITORY https://github.com/pstoecker/DarkAges.git
-    GIT_CONFIG advice.detachedHead=false
     GIT_TAG v1.2.0
     SOURCE_DIR ${dir}
     BUILD_IN_SOURCE 1
