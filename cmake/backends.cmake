1f539ebb6af8645a3ef8c6b6f678fa61bca957db
sanjaybloor <sanjay.bloor12@imperial.ac.uk>
2020-02-19 09:44:03 +1100

    Remove py2 dep. for classy, ditch classy if no cython
    	modified:   cmake/backends.cmake
    	modified:   cmake/optional.cmake

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -1231,11 +1231,6 @@ set(patch "${PROJECT_SOURCE_DIR}/Backends/patches/${name}/${ver}")
 set(dl "https://github.com/lesgourg/class_public/archive/v${ver}.tar.gz")
 set(md5 "e6eb0fd721bb1098e642f5d1970501ce")
 set(dir "${PROJECT_SOURCE_DIR}/Backends/installed/${name}/${ver}")
-# Ditch classy if Python version is 3
-if(NOT PYTHON_VERSION_MAJOR EQUAL 2)
-  message("   CLASSY requires Python2 -- ditching.")
-  set(ditch_if_absent "Python2")
-endif()
 check_ditch_status(${name} ${ver} ${dir} ${ditch_if_absent})
 if(NOT ditched_${name}_${ver})
   ExternalProject_Add(${name}_${ver}
@@ -1265,11 +1260,6 @@ set(patch "${PROJECT_SOURCE_DIR}/Backends/patches/${name}/${ver}")
 set(dl "https://github.com/lesgourg/class_public/archive/067b3868f6501eb68df35554cda5e3981129d48c.tar.gz") # The huge number is the commit ID of ExoCLASS_2.7.0
 set(md5 "6f7d59ac552744fadf47214fde2cbeac")
 set(dir "${PROJECT_SOURCE_DIR}/Backends/installed/${name}/${ver}")
-# Ditch classy if Python version is 3
-if(NOT PYTHON_VERSION_MAJOR EQUAL 2)
-  message("   CLASSY requires Python2 -- ditching.")
-  set(ditch_if_absent "Python2")
-endif()
 check_ditch_status(${name} ${ver} ${dir} ${ditch_if_absent})
 if(NOT ditched_${name}_${ver})
   ExternalProject_Add(${name}_${ver}
