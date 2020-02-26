42f74b960639d67efbd9cfdd74e000f272b8cf1e
janinarenk <janina.renk@fysik.su.se>
2020-02-26 12:32:24 +1000

    Added new MontePython likelihood: WiggleZ_bao_highz to avoid overlapp with BOSS BAO -> can be used as example in paper
    	new file:   Backends/patches/montepythonlike/3.1.0/WiggleZ_bao_highz/WiggleZ_bao_highz.data
    	new file:   Backends/patches/montepythonlike/3.1.0/WiggleZ_bao_highz/__init__.py
    	modified:   cmake/backends.cmake

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -567,6 +567,7 @@ if(NOT ditched_${name}_${ver})
     COMMAND ${CMAKE_COMMAND} -E copy ${patchdir}/MPLike_patch_script.py ${dir}/montepython/MPLike_patch_script.py
     COMMAND sed ${dashi} -e "s#from MontePythonLike import#from MontePythonLike_${sfver} import#g" ${dir}/montepython/MPLike_patch_script.py
     COMMAND ${CMAKE_COMMAND} -E copy ${patchdir}/sdss_lrgDR7_fiducialmodel.dat ${dir}/data/sdss_lrgDR7/sdss_lrgDR7_fiducialmodel.dat
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${patchdir}/WiggleZ_bao_highz ${dir}/montepython/likelihoods/WiggleZ_bao_highz/
     BUILD_COMMAND ""
     INSTALL_COMMAND python ${dir}/montepython/MPLike_patch_script.py
   )
