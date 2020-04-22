a7bce492e8e0b5be8eb3f1fc2060cb01b28055dd
Markus Prim <markus.prim@kit.edu>
2020-04-22 22:11:28 +0900

    Enable escaping of environment variable and clean string from escape characters after expansion of environment variables.

--- Utils/include/gambit/Utils/yaml_options.hpp
+++ Utils/include/gambit/Utils/yaml_options.hpp
@@ -268,27 +268,52 @@ namespace Gambit
         throw std::runtime_error("Reached end of function safeIntegerTypeCast. This should not happen.");
       }
 
-      /// Update the input string.
-      void autoExpandEnvironmentVariables(std::string& text) const {
-        static std::regex env("\\$\\{([^}]+)\\}");
+      /// Expand environment variables in the given string.
+      void autoExpandEnvironmentVariables(std::string & text) const {
+        // C++ regex does not support negative lookahead. So let us reverse the string.
+        std::reverse(text.begin(), text.end());
+
+        // Matches reverse ${MYVAR}, but not \${MYVAR} and $MYVAR
+        const static std::regex env( "\\}([^{]+)\\{\\$(?!\\\\)" );
+
         std::smatch match;
-        while (std::regex_search(text, match, env))
-        {
-          const char* s = getenv(match[1].str().c_str());
-          const std::string var(s == NULL ? "" : s);
-          if (s == NULL)
-          {
-            std::ostringstream os;
-            os << "Environment variable " << match.str() << " not set";
-            utils_error().raise(LOCAL_INFO, os.str());
-          }
-          text.replace(match[0].first, match[0].second, var);
+        while (std::regex_search(text, match, env)) {
+
+            // Reverse the found match into a temporary variable, this is what we actually
+            // want to look for, e.g. we found RAV, but we want to look for VAR.
+            std::string tmp = match[1].str();
+            std::reverse(tmp.begin(), tmp.end());
+
+            // Look for VAR
+            const char * s = std::getenv(tmp.c_str());
+
+            std::string var(s == NULL ? "" : s);
+            if (s == NULL)  {
+              std::cout << "Environment variable " << match.str() << " not set";
+            }
+            // Reverse the found environment variable...
+            std::reverse(var.begin(), var.end());
+            // and plug it into the text string.
+            text.replace( match[0].first, match[0].second, var);
+
         }
+        // Finally return the text string in the normal order.
+        std::reverse(text.begin(), text.end());
       }
 
-      /// Leave input alone and return new string.
+      /// Remove characters in the given string.
+      void removeCharsFromString(std::string& text, const char* charsToRemove ) const {
+         for (unsigned int i = 0; i < strlen(charsToRemove); ++i) {
+            text.erase(std::remove(text.begin(), text.end(), charsToRemove[i]), text.end());
+         }
+      }
+
+      /// Leave input alone and return new string, which has environment variables
+      /// substituted and escpae characters removed.
       std::string expandEnvironmentVariables(const std::string& input ) const {
+        static const char* escape_character = "\\";
         std::string text = input;
+        removeCharsFromString( text, escape_character );
         autoExpandEnvironmentVariables(text);
         return text;
       }
