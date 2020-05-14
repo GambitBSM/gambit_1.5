b111d34698f57d5e0f9ec1331ca582907910ae53
Patrick Stöcker <patrick.stoecker@rwth-aachen.de>
2020-05-14 18:48:31 +0200

    Add global "print_unitcube" option for printer
      * This option (boolean flag) is attribute of BaseBasePrinter.
      * Defaults to TRUE (UnitCubeParameters are printed if not stated otherwise in the yaml-file)
      * The value from the input is forwarded to the printer via the dependency resolver
      * Printing of unitCubeParameters is suppressed, when flag is false OR dimension of UnitCubeParameters is zero
    
    	modified:       Core/include/gambit/Core/depresolver.hpp
    	modified:       Core/src/depresolver.cpp
    	modified:       Printers/include/gambit/Printers/basebaseprinter.hpp
    	modified:       ScannerBit/include/gambit/ScannerBit/factory_defs.hpp

--- Core/include/gambit/Core/depresolver.hpp
+++ Core/include/gambit/Core/depresolver.hpp
@@ -22,6 +22,10 @@
 ///          (tomas.gonzalo@monash.edu)
 ///  \date 2019 May
 ///
+///  \author Patrick Stoecker
+///          (stoecker@physik.rwth-aachen.de)
+///  \date 2020 May
+///
 ///  *********************************************
 
 #ifndef __depresolver_hpp__
@@ -274,6 +278,8 @@ namespace Gambit
         /// Global flag for triggering printing of timing data
         bool print_timing = false;
 
+        /// Global flag for triggering printing of unitCubeParameters
+        bool print_unitcube = true;
   };
   }
 }
