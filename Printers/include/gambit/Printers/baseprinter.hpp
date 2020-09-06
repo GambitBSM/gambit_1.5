GambitBSM/gambit_1.5

commit f92ba99c36444f0f3bf323f02ae290c9562d0f81

Fixed issue with BBN_container preventing compilation of ScannerBit_standalone 	modified:   Backends/include/gambit/Backends/backend_types/AlterBBN.hpp 	deleted:    Backends/src/backend_types/AlterBBN.cpp 	modified:   Printers/include/gambit/Printers/baseprinter.hpp 	modified:   Printers/include/gambit/Printers/printers/asciitypes.hpp 	modified:   Printers/include/gambit/Printers/printers/sqlitereader.hpp 	modified:   Printers/include/gambit/Printers/printers/sqlitetypes.hpp 	modified:   Printers/src/printers/coutprinter/print_overloads.cpp 	modified:   Printers/src/printers/sqliteprinter/print_overloads.cpp

Printers/include/gambit/Printers/baseprinter.hpp
@@ -34,8 +34,6 @@
 #include "gambit/Utils/model_parameters.hpp"
 #include "gambit/Utils/export_symbols.hpp"
 
-#include "gambit/Logs/logmaster.hpp"
-
 // Boost
 #include <boost/preprocessor/seq/for_each_i.hpp>
 

Backends/include/gambit/Backends/backend_types/AlterBBN.hpp
@@ -36,7 +36,9 @@
 namespace Gambit
 {
 
-  /// Class to store all results from an AlterBBN run
+  /// Class to store all results from an AlterBBN run.
+  /// This class needs to be fully defined in the header in order to prevent
+  /// linking problems when building the ScannerBit standalone.
   /// -> element abundances stored in BBN_nuc (length NNUC+1),
   /// -> covariance matrix in BBN_covmat ( dim NNUC+1 x NNUC+1)
   /// -> abund_map maps name of element to position in BBN_abundance vector
@@ -45,43 +47,61 @@ namespace Gambit
   {
     public:
       /// Constructor
-      BBN_container();
+      BBN_container() : abund_map{{"H2",3}, {"D",3}, {"H3",4}, {"He3",5}, {"He4",6}, {"Yp",6}, {"Li6",7}, {"Li7",8}, {"Be7",9}, {"Li8",10}}
+      {}
 
       /// Initialize sizes of vectors (get NNUC, number of computed element abundances, from AlterBBN)
-      void init_arr_size(size_t);
+      void init_arr_size(size_t nnuc)
+      {
+        NNUC = nnuc;
+        BBN_abund.resize(NNUC+1, 0.);
+        BBN_covmat.resize(NNUC+1, std::vector<double>(NNUC+1,0.));
+      }
 
       /// Initialise the translation map from element name to position in abundance vector
-      void set_abund_map(map_str_int);
+      void set_abund_map(map_str_int map_in) {abund_map = map_in;}
 
       /// Setter functions for abundance vector
-      void set_BBN_abund(int, double);
+      void set_BBN_abund(int pos, double val) {BBN_abund[pos] = val;}
 
       /// Setter function for covariance matrix
-      void set_BBN_covmat(int, int, double);
+      void set_BBN_covmat(int row, int col, double val) {BBN_covmat[row][col] = val;}
 
       /// Global parameter in AlterBBN; holds number of computed element abundances
-      size_t get_NNUC() const;
+      size_t get_NNUC() const {return NNUC;};
 
       /// Getter for map from isotope names to position in BBN_abundance vector
-      const std::map<str,int>& get_abund_map() const;
+      const std::map<std::string,int>& get_abund_map() const {return abund_map;};
 
       /// Getter for abundance
-      double get_BBN_abund(int pos) const;
+      double get_BBN_abund(int pos) const {return BBN_abund[pos];}
 
       /// Getter for abundance
-      double get_BBN_abund(str iso) const;
+      double get_BBN_abund(str iso) const {return BBN_abund[abund_map.at(iso)];}
 
       /// Getter for covariance matrix
-      double get_BBN_covmat(int, int) const;
+      double get_BBN_covmat(int row, int col) const {return BBN_covmat[row][col];}
 
       /// Setter for active isotopes
-      void set_active_isotopes(std::set<str>);
+      void set_active_isotopes(std::set<str> isos)
+      {
+        active_isotopes.clear();
+        active_isotope_indices.clear();
+        for (const str& s : isos)
+        {
+          if (abund_map.find(s) != abund_map.end())
+          {
+           active_isotopes.insert(s);
+           active_isotope_indices.insert(abund_map.at(s));
+          }
+        }
+      }
 
       /// Getter for active isotopes
-      const std::set<str>& get_active_isotopes() const;
+      const std::set<str>& get_active_isotopes() const {return active_isotopes;}
 
       /// Getter for indices of active isotopes in BBN_abundance vector
-      const std::set<int>& get_active_isotope_indices() const;
+      const std::set<int>& get_active_isotope_indices() const {return active_isotope_indices;}
 
     private:
       size_t NNUC;

Backends/src/backend_types/AlterBBN.cpp
@@ -1,86 +1,0 @@
-//   GAMBIT: Global and Modular BSM Inference Tool
-//   *********************************************
-///  \file
-///
-///  Definitions of containers for BBN
-///  calculations.
-///
-///  *********************************************
-///
-///  Authors (add name and date if you modify):
-///
-///  \author Janina Renk
-///          (janina.renk@fysik.su.se)
-///  \date 2019 Mar
-///
-///  \author Pat Scott
-///          (pat.scott@uq.edu.au)
-///  \date 2020 Apr
-///
-///  *********************************************
-
-#include "gambit/Backends/backend_types/AlterBBN.hpp"
-
-
-namespace Gambit
-{
-
-  /// Constructor
-  BBN_container::BBN_container() : abund_map{{"H2",3}, {"D",3}, {"H3",4}, {"He3",5}, {"He4",6}, {"Yp",6}, {"Li6",7}, {"Li7",8}, {"Be7",9}, {"Li8",10}}
-  {}
-
-  /// Initialize sizes of vectors (get NNUC, number of computed element abundances, from AlterBBN)
-  void BBN_container::init_arr_size(size_t nnuc)
-  {
-    NNUC = nnuc;
-    BBN_abund.resize(NNUC+1, 0.);
-    BBN_covmat.resize(NNUC+1, std::vector<double>(NNUC+1,0.));
-  }
-
-  /// Initialise the translation map from element name to position in abundance vector
-  void BBN_container::set_abund_map(map_str_int map_in) {abund_map = map_in;}
-
-  /// Setter functions for abundance vector
-  void BBN_container::set_BBN_abund(int pos, double val) {BBN_abund[pos] = val;}
-
-  /// Setter function for covariance matrix
-  void BBN_container::set_BBN_covmat(int row, int col, double val) {BBN_covmat[row][col] = val;}
-
-  /// Global parameter in AlterBBN; holds number of computed element abundances
-  size_t BBN_container::get_NNUC() const {return NNUC;};
-
-  /// Getter for map from isotope names to position in BBN_abundance vector
-  const std::map<std::string,int>& BBN_container::get_abund_map() const {return abund_map;};
-
-  /// Getter for abundance
-  double BBN_container::get_BBN_abund(int pos) const {return BBN_abund[pos];}
-
-  /// Getter for abundance
-  double BBN_container::get_BBN_abund(str iso) const {return BBN_abund[abund_map.at(iso)];}
-
-  /// Getter for covariance matrix
-  double BBN_container::get_BBN_covmat(int row, int col) const {return BBN_covmat[row][col];}
-
-  /// Setter for active isotopes
-  void BBN_container::set_active_isotopes(std::set<str> isos)
-  {
-    active_isotopes.clear();
-    active_isotope_indices.clear();
-    for (const str& s : isos)
-    {
-      if (abund_map.find(s) != abund_map.end())
-      {
-       active_isotopes.insert(s);
-       active_isotope_indices.insert(abund_map.at(s));
-      }
-    }
-  }
-
-  /// Getter for active isotopes
-  const std::set<str>& BBN_container::get_active_isotopes() const {return active_isotopes;}
-
-  /// Getter for indices of active isotopes in BBN_abundance vector
-  const std::set<int>& BBN_container::get_active_isotope_indices() const {return active_isotope_indices;}
-
-}
-

Printers/include/gambit/Printers/printers/asciitypes.hpp
@@ -31,6 +31,6 @@
   (DM_nucleon_couplings)                    \
   (DM_nucleon_couplings_fermionic_HP)       \
   (Flav_KstarMuMu_obs)                      \
-  (BBN_container)
+  (BBN_container)                           \
 
 #endif

Printers/include/gambit/Printers/printers/sqlitereader.hpp
@@ -55,9 +55,9 @@ namespace Gambit
         /// Retrieve functions
         using BaseReader::_retrieve; // Tell compiler we are using some of the base class overloads of this on purpose.
         #define DECLARE_RETRIEVE(r,data,i,elem) bool _retrieve(elem&, const std::string&, const uint, const ulong);
-        BOOST_PP_SEQ_FOR_EACH_I(DECLARE_RETRIEVE, , SQLITE_TYPES)
+        BOOST_PP_SEQ_FOR_EACH_I(DECLARE_RETRIEVE, , SQL_TYPES)
         #ifndef SCANNER_STANDALONE
-          BOOST_PP_SEQ_FOR_EACH_I(DECLARE_RETRIEVE, , SQLITE_BACKEND_TYPES)
+          BOOST_PP_SEQ_FOR_EACH_I(DECLARE_RETRIEVE, , SQL_BACKEND_TYPES)
         #endif
         #undef DECLARE_RETRIEVE
 

Printers/include/gambit/Printers/printers/sqlitetypes.hpp
@@ -18,7 +18,7 @@
 #ifndef __SQLITETYPES__
 #define __SQLITETYPES__
 
-#define SQLITE_TYPES        \
+#define SQL_TYPES           \
   (int)                     \
   (uint)                    \
   (long)                    \
@@ -35,7 +35,7 @@
   (map_intpair_dbl)         \
 
 
-#define SQLITE_BACKEND_TYPES          \
+#define SQL_BACKEND_TYPES             \
   (DM_nucleon_couplings)              \
   (DM_nucleon_couplings_fermionic_HP) \
   (Flav_KstarMuMu_obs)                \

Printers/src/printers/coutprinter/print_overloads.cpp
@@ -74,5 +74,3 @@ namespace Gambit
   }
 }
 
-#undef DBUG
-#undef DEBUG_MODE

Printers/src/printers/sqliteprinter/print_overloads.cpp
@@ -21,8 +21,6 @@
 #include "gambit/Printers/printers/sqliteprinter.hpp"
 #include "gambit/Printers/printers/common_print_overloads.hpp"
 
-//#include "gambit/Utils/stream_overloads.hpp"
-
 namespace Gambit
 {
   namespace Printers
@@ -63,5 +61,3 @@ namespace Gambit
   }
 }
 
-#undef DBUG
-#undef DEBUG_MODE

commit 61be211cc0db836647f54aa826db7a0848d90555

Fix compile problem (incomplete type declaration) introduced in f92ba99
	modified:       Printers/include/gambit/Printers/baseprinter.hpp

Printers/include/gambit/Printers/baseprinter.hpp
@@ -34,6 +34,8 @@
 #include "gambit/Utils/model_parameters.hpp"
 #include "gambit/Utils/export_symbols.hpp"
 
+#include "gambit/Logs/logmaster.hpp"
+
 // Boost
 #include <boost/preprocessor/seq/for_each_i.hpp>
 